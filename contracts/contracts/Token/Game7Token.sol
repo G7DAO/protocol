0d36a971db30ede2ee40a58cf1b678e199d1de86
Daniel Lima <daniel.lima@orium.network>
2024-05-23 16:41:48 -0300

    Feat: Add ERC20 interface and implementation

---
+++ contracts/contracts/Token/Game7Token.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.24;
+
+import { IERC20 } from './interfaces/IERC20.sol';
+
+/**
+ * @title Game7 Token
+ * @author Game7 Engineering Team - engineering@game7.io
+ */
+contract Game7Token is IERC20 {
+    string public constant name = 'Game7 Token';
+    string public constant symbol = 'G7T';
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    constructor(uint256 _totalSupply) {
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        return transferFrom(msg.sender, to, value);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value);
+
+        if (msg.sender != from && allowance[from][msg.sender] != 0) {
+            require(allowance[from][msg.sender] >= value);
+            allowance[from][msg.sender] -= value;
+        }
+
+        balanceOf[from] -= value;
+        balanceOf[to] -= value;
+
+        emit Transfer(from, to, value);
+
+        return true;
+    }
+
+}
