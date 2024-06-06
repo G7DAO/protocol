b5885d4ed9f344783b95842bdea8c033e04bf9b5
Kellan Wampler <kellan@moonstream.to>
2024-06-06 01:53:48 -0400

    ReentrancyGuard on deposit and withdraw.

--- web3/contracts/staking/StakingTokens.sol
+++ web3/contracts/staking/StakingTokens.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.24;
 import { IERC20 } from '../interfaces/IERC20.sol';
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title Game7 Staking
  * @author Game7 Engineering Team - engineering@game7.io
  */
-contract StakingTokens is ERC721Enumerable {
+contract StakingTokens is ERC721Enumerable, ReentrancyGuard {
 
     struct Deposit {
         address tokenAddress;
@@ -31,7 +32,7 @@ contract StakingTokens is ERC721Enumerable {
         return _allDeposits[_tokenID];
     }
 
-    function deposit(address _tokenAddress, uint256 _amount, uint256 _duration, address _receiver) external {
+    function deposit(address _tokenAddress, uint256 _amount, uint256 _duration, address _receiver) external nonReentrant {
         require(_receiver != address(0), "Staking.deposit: receiver cannot be zero address");
         require(_amount > 0, "Staking.deposit: cannot deposit 0");
 
@@ -54,7 +55,7 @@ contract StakingTokens is ERC721Enumerable {
         emit Deposited(_tokenAddress, _receiver, msg.sender, _duration, _amount);
     }
 
-    function withdraw(uint256 _tokenID, address _receiver) external {
+    function withdraw(uint256 _tokenID, address _receiver) external nonReentrant {
         require(_receiver != address(0), "Staking.withdraw: receiver cannot be zero address");
         require(ownerOf(_tokenID) == msg.sender, "Staking.withdraw: deposit does not exist");
 
