cb41134a53a08eea8c0fc8441aaed0facec599f9
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-13 00:46:20 +0300

    ValueToBridge component

---
+++ webapps/world-builder-dashboard/src/components/bridge/ValueToBridge.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from "./BridgeView.module.css";
+
+const balanceString = (balance: string | undefined, symbol: string) => {
+    return `${balance ?? '0'} ${symbol} Available`;
+}
+
+const formatCurrency = (value: number) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+    return formatter.format(value);
+}
+
+interface ValueToBridgeProps {
+    title: string,
+    symbol: string,
+    value: string,
+    setValue: (value: string) => void,
+    balance: string | undefined,
+    rate: number,
+}
+const ValueToBridge: React.FC<ValueToBridgeProps> = ({title, setValue, value, balance, symbol, rate}) => {
+
+    return (
+     <div className={styles.valueBlockContainer}>
+          <div className={styles.valueContainer}>
+            <div className={styles.label}>{title}</div>
+            <input  className={styles.valueInput} value={value} onChange={(e) => setValue(e.target.value)} />
+              <div className={styles.balance}>{formatCurrency(Number(value) * rate)}</div>
+          </div>
+          <div className={styles.symbolContainer}>
+              {symbol && <div className={styles.symbol}>{symbol}</div>}
+              <div className={styles.balanceContainer}>
+                  <div className={styles.balance}>{balanceString(balance, symbol)}</div>
+                  <button type={'button'} className={styles.maxButton} onClick={() => setValue(String(balance))}>MAX</button>
+              </div>
+          </div>
+      </div>
+      );
+};
+
+export default ValueToBridge;
