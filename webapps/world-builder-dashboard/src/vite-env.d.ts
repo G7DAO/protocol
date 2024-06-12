1d498c25b28081f2c3847c1672dbeaf3d6b29f7e
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-13 00:44:38 +0300

    extending window.ethereum type

--- webapps/world-builder-dashboard/src/vite-env.d.ts
+++ webapps/world-builder-dashboard/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+// types/ethereum.d.ts
+
+import { ExternalProvider } from '@ethersproject/providers';
+
+export interface Ethereumish extends ExternalProvider {
+    isMetaMask?: boolean;
+    on?: (event: string, listener: (...args: any[]) => void) => void;
+    removeListener?: (event: string, listener: (...args: any[]) => void) => void;
+}
+
+declare global {
+    interface Window {
+        ethereum?: Ethereumish;
+    }
+}
