6908b5757ae90deb2d3dd81e89be18d740b0ebec
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-13 14:19:28 +0300

    L2 network parameters

---
+++ webapps/world-builder-dashboard/constants.ts
@@ -0,0 +1,14 @@
+import {ChainInterface} from "./src/components/bridge/BlockchainContext";
+
+export const L2_CHAIN: ChainInterface = {
+    chainId: 421614,
+    name: 'arbitrumSepolia',
+    displayName: 'Arbitrum Sepolia',
+    rpcs: ['https://sepolia-rollup.arbitrum.io/rpc'],
+    blockExplorerUrls: ['https://sepolia.arbiscan.io/'],
+    nativeCurrency: {
+        decimals: 18,
+        name: 'ETH',
+        symbol: 'ETH',
+    },
+}
