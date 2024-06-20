6aef12ad52afd36e381a618eb636e06a760cd2df
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-20 13:45:48 +0300

    hook to fetch statuses for array of messages

--- webapps/world-builder-dashboard/src/hooks/useL2ToL1MessageStatus.ts
+++ webapps/world-builder-dashboard/src/hooks/useL2ToL1MessageStatus.ts
@@ -1,4 +1,4 @@
-import {useQuery} from "react-query";
+import {useQuery, useQueries} from "react-query";
 import {ethers} from "ethers";
 import {L2ToL1MessageReader, L2ToL1MessageStatus, L2TransactionReceipt} from "@arbitrum/sdk";
 
@@ -18,6 +18,16 @@ const eventABI = [{
     name: "L2ToL1Tx",
     type: "event",
 }];
+
+export interface L2ToL1MessageStatusResult {
+    from?: string;
+    to?: string;
+    value?: string;
+    timestamp?: number;
+    confirmations?: number;
+    status?: L2ToL1MessageStatus;
+    l2Receipt?: L2TransactionReceipt;
+}
 
 const useL2ToL1MessageStatus = (txHash: string, l2RPC: string, l3RPC: string) => {
     return useQuery(
@@ -58,6 +68,57 @@ const useL2ToL1MessageStatus = (txHash: string, l2RPC: string, l3RPC: string) =>
         {
             refetchInterval: 60000 * 3,
         }
+    );
+};
+
+
+export interface Transaction {
+    txHash: string;
+    l2RPC: string;
+    l3RPC: string;
+}
+
+export const useL2ToL1MessagesStatus = (transactions: Transaction[] | undefined) => {
+    if (!transactions) {
+        return useQueries([{queryKey: ["withdrawalStatusEmpty"], queryFn: () => undefined}])
+    }
+    return useQueries(
+        transactions.map(({ txHash, l2RPC, l3RPC }) => ({
+            queryKey: ["withdrawalStatus", txHash, l2RPC, l3RPC],
+            queryFn: async () => {
+                console.log("checking status", txHash.slice(0, 6), l2RPC, l3RPC);
+                const l3Provider = new ethers.providers.JsonRpcProvider(l3RPC);
+                const l2Provider = new ethers.providers.JsonRpcProvider(l2RPC);
+                const receipt = await l3Provider.getTransactionReceipt(txHash);
+                const l2Receipt = new L2TransactionReceipt(receipt);
+                const log = receipt.logs.find((l) => l.data !== "0x");
+                let decodedLog;
+
+                if (log) {
+                    try {
+                        const iface = new ethers.utils.Interface(eventABI);
+                        decodedLog = iface.parseLog(log);
+                    } catch (e) {
+                        console.log(log, e);
+                    }
+                }
+
+                const messages: L2ToL1MessageReader[] = (await l2Receipt.getL2ToL1Messages(l2Provider)) as L2ToL1MessageReader[];
+                const l2ToL1Msg: L2ToL1MessageReader = messages[0];
+                const status: L2ToL1MessageStatus = await l2ToL1Msg.status(l3Provider);
+
+                return {
+                    from: decodedLog?.args?.caller,
+                    to: decodedLog?.args?.destination,
+                    value: ethers.utils.formatEther(decodedLog?.args?.callvalue ?? "0"),
+                    timestamp: decodedLog?.args?.timestamp,
+                    confirmations: receipt.confirmations,
+                    status,
+                    l2Receipt,
+                };
+            },
+            refetchInterval: 60000 * 3,
+        }))
     );
 };
 
