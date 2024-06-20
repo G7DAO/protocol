691a6f1cd5a8afda8f4f7cf8f330779a580639b0
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-20 13:38:37 +0300

    removing header from bridge page

--- webapps/world-builder-dashboard/src/pages/BridgePage/BridgePage.tsx
+++ webapps/world-builder-dashboard/src/pages/BridgePage/BridgePage.tsx
@@ -1,24 +1,18 @@
 import { ContentHeader } from "summon-ui";
 import { Box } from "summon-ui/mantine";
-import {useLocation, useNavigate} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import BridgeView from "@/components/bridge/BridgeView";
 import {BlockchainProvider} from "@/components/bridge/BlockchainContext";
 import WithdrawTransactions from "@/components/bridge/WithdrawTransactions";
 
-const BRIDGE_TABS = [
-    {name: "Bridge", to: "/bridge"},
-    {name: "Transaction history", to: "/bridge/transactions"}
-]
 
 const BridgePage = () => {
-    const navigate = useNavigate()
     const location = useLocation();
-    const list = BRIDGE_TABS.map((tab) => ({ name: tab.name, to: tab.to, action: () => navigate(tab.to) }))
 
     return (
         <BlockchainProvider>
-            <Box px="md">
-                <ContentHeader name='Bridge' tabs={{ list, value: location.pathname }}>
+            <Box px="md" bg={'#FFF'} h={'100vh'}>
+                <ContentHeader name='' tabs={{ list: [], value: location.pathname }}>
                 </ContentHeader>
                 {location.pathname === '/bridge' && <BridgeView />}
                 {location.pathname === '/bridge/transactions' && <WithdrawTransactions />}
