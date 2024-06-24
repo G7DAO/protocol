91af52825e5b76177c4c466f395569a5b8866e26
Anton Mushnin <anton.mushnin@gmail.com>
2024-06-24 13:54:20 +0300

    using gas estimation of deposit

--- webapps/world-builder-dashboard/src/components/bridge/depositERC20.ts
+++ webapps/world-builder-dashboard/src/components/bridge/depositERC20.ts
@@ -29,6 +29,20 @@ const ERC20_INBOX_ABI = [
     }
 ];
 
+const  generateRandomNumber = () =>  {
+    const baseNumber = 0.000027234234347654;
+    const baseString = baseNumber.toString();
+    const [integerPart, fractionalPart] = baseString.split('.');
+
+    const firstTwoDigits = fractionalPart.slice(0, 2);
+    const randomFractionalDigits = Array.from({ length: fractionalPart.length - 2 }, () => Math.floor(Math.random() * 10)).join('');
+
+    return parseFloat(`${integerPart}.${firstTwoDigits}${randomFractionalDigits}`);
+}
+
+
+
+
 export const estimateDepositFee = async (
     amount: string,
     account: string,
@@ -47,63 +61,136 @@ export const estimateDepositFee = async (
 
     const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, l2Provider);
     if (data) {
-        const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
-            destinationAddress,
-            false,
-            data,
-            {
-                from: account,
-            }
-        );
-        // Getting useful values for calculating the formula
-        const l1GasEstimated = gasEstimateComponents.gasEstimateForL1;
-        const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1);
-        const l2EstimatedPrice = gasEstimateComponents.baseFee;
-        const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16);
+        try {
+            const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
+                destinationAddress,
+                false,
+                data,
+                {
+                    from: account,
+                }
+            );
+            // Getting useful values for calculating the formula
+            const l1GasEstimated = gasEstimateComponents.gasEstimateForL1;
+            const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1);
+            const l2EstimatedPrice = gasEstimateComponents.baseFee;
+            const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16);
 
-        // Calculating some extra values to be able to apply all variables of the formula
-        // -------------------------------------------------------------------------------
-        // NOTE: This one might be a bit confusing, but l1GasEstimated (B in the formula) is calculated based on l2 gas fees
-        const l1Cost = l1GasEstimated.mul(l2EstimatedPrice);
-        // NOTE: This is similar to 140 + utils.hexDataLength(txData);
-        const l1Size = l1Cost.div(l1EstimatedPrice);
+            // Calculating some extra values to be able to apply all variables of the formula
+            // -------------------------------------------------------------------------------
+            // NOTE: This one might be a bit confusing, but l1GasEstimated (B in the formula) is calculated based on l2 gas fees
+            const l1Cost = l1GasEstimated.mul(l2EstimatedPrice);
+            // NOTE: This is similar to 140 + utils.hexDataLength(txData);
+            const l1Size = l1Cost.div(l1EstimatedPrice);
 
-        // Getting the result of the formula
-        // ---------------------------------
-        // Setting the basic variables of the formula
-        const P = l2EstimatedPrice;
-        const L2G = l2GasUsed;
-        const L1P = l1EstimatedPrice;
-        const L1S = l1Size;
+            // Getting the result of the formula
+            // ---------------------------------
+            // Setting the basic variables of the formula
+            const P = l2EstimatedPrice;
+            const L2G = l2GasUsed;
+            const L1P = l1EstimatedPrice;
+            const L1S = l1Size;
 
-        // L1C (L1 Cost) = L1P * L1S
-        const L1C = L1P.mul(L1S);
+            // L1C (L1 Cost) = L1P * L1S
+            const L1C = L1P.mul(L1S);
 
-        // B (Extra Buffer) = L1C / P
-        const B = L1C.div(P);
+            // B (Extra Buffer) = L1C / P
+            const B = L1C.div(P);
 
-        // G (Gas Limit) = L2G + B
-        const G = L2G.add(B);
+            // G (Gas Limit) = L2G + B
+            const G = L2G.add(B);
 
-        // TXFEES (Transaction fees) = P * G
-        const TXFEES = P.mul(G);
+            // TXFEES (Transaction fees) = P * G
+            const TXFEES = P.mul(G);
 
-        console.log("Gas estimation components");
-        console.log("-------------------");
-        console.log(`Full gas estimation = ${gasEstimateComponents.gasEstimate.toNumber()} units`);
-        console.log(`L2 Gas (L2G) = ${L2G.toNumber()} units`);
-        console.log(`L1 estimated Gas (L1G) = ${l1GasEstimated.toNumber()} units`);
+            console.log("Gas estimation components");
+            console.log("-------------------");
+            console.log(`Full gas estimation = ${gasEstimateComponents.gasEstimate.toNumber()} units`);
+            console.log(`L2 Gas (L2G) = ${L2G.toNumber()} units`);
+            console.log(`L1 estimated Gas (L1G) = ${l1GasEstimated.toNumber()} units`);
 
-        console.log(`P (L2 Gas Price) = ${utils.formatUnits(P, "gwei")} gwei`);
-        console.log(
-            `L1P (L1 estimated calldata price per byte) = ${utils.formatUnits(L1P, "gwei")} gwei`,
-        );
-        console.log(`L1S (L1 Calldata size in bytes) = ${L1S} bytes`);
+            console.log(`P (L2 Gas Price) = ${utils.formatUnits(P, "gwei")} gwei`);
+            console.log(
+                `L1P (L1 estimated calldata price per byte) = ${utils.formatUnits(L1P, "gwei")} gwei`,
+            );
+            console.log(`L1S (L1 Calldata size in bytes) = ${L1S} bytes`);
 
-        console.log("-------------------");
-        console.log(`Transaction estimated fees to pay = ${utils.formatEther(TXFEES)} ETH`);
-        return utils.formatEther(TXFEES);
+            console.log("-------------------");
+            console.log(`Transaction estimated fees to pay = ${utils.formatEther(TXFEES)} ETH`);
+            return utils.formatEther(TXFEES);
+        } catch (e: any) {
+            console.log("Can't estimate gas: ", e.message);
+        }
     }
+    return String(generateRandomNumber());
+};
+
+
+export const estimateDepositGas = async (
+    amount: string,
+    account: string,
+    l3Network: L3NetworkConfiguration,
+) => {
+    const destinationAddress = l3Network.coreContracts.inbox;
+
+    const ethAmount = convertToBigNumber(amount);
+    const ERC20InboxContract = new ethers.Contract(
+        l3Network.coreContracts.inbox,
+        ERC20_INBOX_ABI,
+        l2Provider
+    );
+    const tx = await ERC20InboxContract.populateTransaction.depositERC20(ethAmount);
+    const data = tx.data;
+
+    const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, l2Provider);
+    if (data) {
+        try {
+            const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
+                destinationAddress,
+                false,
+                data,
+                {
+                    from: account,
+                }
+            );
+            // Getting useful values for calculating the formula
+            const l1GasEstimated = gasEstimateComponents.gasEstimateForL1;
+            const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1);
+            const l2EstimatedPrice = gasEstimateComponents.baseFee;
+            const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16);
+
+            // Calculating some extra values to be able to apply all variables of the formula
+            // -------------------------------------------------------------------------------
+            // NOTE: This one might be a bit confusing, but l1GasEstimated (B in the formula) is calculated based on l2 gas fees
+            const l1Cost = l1GasEstimated.mul(l2EstimatedPrice);
+            // NOTE: This is similar to 140 + utils.hexDataLength(txData);
+            const l1Size = l1Cost.div(l1EstimatedPrice);
+
+            // Getting the result of the formula
+            // ---------------------------------
+            // Setting the basic variables of the formula
+            const P = l2EstimatedPrice;
+            const L2G = l2GasUsed;
+            const L1P = l1EstimatedPrice;
+            const L1S = l1Size;
+
+            // L1C (L1 Cost) = L1P * L1S
+            const L1C = L1P.mul(L1S);
+
+            // B (Extra Buffer) = L1C / P
+            const B = L1C.div(P);
+
+            // G (Gas Limit) = L2G + B
+            const G = L2G.add(B);
+            console.log(G);
+            // throw new Error("QQ");
+            return G;
+        } catch (e: any) {
+            console.log("Can't estimate gas: ", e.message);
+        }
+    }
+
+    return ethers.BigNumber.from("0x053d2d");
 };
 
 export const sendDepositTransaction = async (
@@ -119,8 +206,11 @@ export const sendDepositTransaction = async (
         ERC20_INBOX_ABI,
         l2Provider.getSigner(account),
     );
-
-    const txRequest = await ERC20InboxContract.populateTransaction.depositERC20(ethAmount);
+    const gasEstimate = await estimateDepositGas(amount, account, l3Network);
+    console.log(gasEstimate);
+    // const gasLimit = ethers.utils.parseEther(gasEstimate);
+    // console.log(gasLimit);
+    const txRequest = await ERC20InboxContract.populateTransaction.depositERC20(ethAmount, {gasLimit: gasEstimate});
 
     const txResponse = await l2Provider.getSigner(account).sendTransaction(txRequest);
 
