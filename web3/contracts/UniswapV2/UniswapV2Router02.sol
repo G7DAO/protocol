4ded0d807e6c0c69be7f0b9bdd265ffcabc00d2b
mrk-hub <mrkennedy@game7.io>
2024-07-08 15:17:30 -0400

    Updated Wrapper1155Factory and added Interface

--- web3/contracts/UniswapV2/UniswapV2Router02.sol
+++ web3/contracts/UniswapV2/UniswapV2Router02.sol
@@ -5,7 +5,12 @@ import '@uniswap/v2-periphery/contracts/UniswapV2Router02.sol';
 
 contract Game7Router is UniswapV2Router02 {
 
-    constructor(address _factory, address _WETH) UniswapV2Router02(_factory, _WETH) public {}
+    address public immutable wrapperFactory;
+
+    constructor(address _v2Factory, address _WETH, address _wrapperFactory) UniswapV2Router02(_v2Factory, _WETH) public {
+        wrapperFactory = _wrapperFactory;
+
+    }
 
 
 }
--- web3/contracts/wrapper/Wrapper1155Factory.sol
+++ web3/contracts/wrapper/Wrapper1155Factory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "../token/DEXW1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-contract Factory {
+contract Wrapper1155Factory {
 
     // 1155 to tokenid to erc20
     mapping(address =>mapping(uint256 => address)) _get20;
@@ -17,12 +17,21 @@ contract Factory {
     }
     constructor(){}
 
+    function get1155Wrapper(address _contract, uint256 tokenid) external view returns(address token){
+        token = _get20[_contract][tokenid];
+    }
+
+    function get1155TokenID(address token) external view returns(address _contract, uint256 tokenid){
+        _contract = _getData[token]._contract;
+        tokenid = _getData[token].tokenId;
+    }
+
     function create20(address _contract, uint256 tokenId)public returns(address token){
         token = _get20[_contract][tokenId];
         if(token != address(0)){
             //need nameing schematic
-            string memory _name = "Token 1";
-            string memory _symbol = "TKN1";
+            string memory _name = string(abi.encodePacked(_contract));
+            string memory _symbol = string(abi.encodePacked(tokenId));
             bytes memory bytecode = type(DEXW1155).creationCode;
 
             bytes32 salt = keccak256(abi.encodePacked(_name, _symbol, _contract, tokenId));
