717b650a8b82236bd4c4fc00bcd41a41a8759089
Anton Mushnin <anton.mushnin@gmail.com>
2024-07-16 16:06:30 +0300

    adding new G7 test network

--- webapps/world-builder-dashboard/src/components/bridge/l3Networks.ts
+++ webapps/world-builder-dashboard/src/components/bridge/l3Networks.ts
@@ -136,5 +136,51 @@ export const L3_NETWORKS = [
         wethGateway: '0x0000000000000000000000000000000000000000'
       }
     }
+  },
+  {
+    chainInfo: {
+      minL2BaseFee: 10000000,
+      networkFeeReceiver: '0x252431e84d5e22435a0c833c2220770c52f59633',
+      infrastructureFeeCollector: '0x252431e84d5e22435a0c833c2220770c52f59633',
+      batchPoster: '0x8d77fDe7c1c812877631a6f7Efc9036dcB6481AC',
+      staker: '0xc2d08c2CCFdB58e6d5b682bD51D617C049CeE9d4',
+      chainOwner: '0x58f425D057C905D976601986831870B0044f9715',
+      chainName: 'conduit-orbit-deployer',
+      chainId: 13746,
+      parentChainId: 421614,
+      nativeToken: '0x10adBf84548F923577Be12146eAc104C899D1E75',
+      rpcs: ['https://rpc-game7-testnet-0ilneybprf.t.conduit.xyz']
+    },
+    coreContracts: {
+      rollup: '0x6cf5bFffc54cDd13B4747e8DF2C72ce8A95043c0',
+      inbox: '0xE6470bb72291c39073AEd67a30ff93B69c1f47De',
+      outbox: '0x64105c6C3D494469D5F21323F0E917563489d9f5',
+      adminProxy: '0x8767Ea2Ce21ac4e624F8a36948BD5EA23A3288D9',
+      sequencerInbox: '0xAe2caC32b0eF386Ab683459648eDFC78F7FF8F1e',
+      bridge: '0xC7EEB897bA9bc3fA071C3871e7F4Cf1Ae7570f16',
+      utils: '0xB11EB62DD2B352886A4530A9106fE427844D515f',
+      validatorWalletCreator: '0xEb9885B6c0e117D339F47585cC06a2765AaE2E0b',
+      l3UpgradeExecutor: '0x8d2821B57E7B3D860E250C0bdc7Cb924B17C72b2'
+    },
+    tokenBridgeContracts: {
+      l2Contracts: {
+        customGateway: '0x81aCB22000A2A81D26E7e1ed5a8f51930A31598E',
+        multicall: '0xce1CAd780c529e66e3aa6D952a1ED9A6447791c1',
+        proxyAdmin: '0x8767Ea2Ce21ac4e624F8a36948BD5EA23A3288D9',
+        router: '0x73EeAEEC11473534a2249c851e4b245E61Da8732',
+        standardGateway: '0x4A24f98D6fB62Ce8eA8f6C2D5AF9c8BF1c853fD7',
+        weth: '0x0000000000000000000000000000000000000000',
+        wethGateway: '0x0000000000000000000000000000000000000000'
+      },
+      l3Contracts: {
+        customGateway: '0xe6c5Ab297E022A592a3fF26984cc6352C7cD0f92',
+        multicall: '0x27c4a2f1B1685F0AD1ea2227F56606066Aa95Bd0',
+        proxyAdmin: '0x07424574dbF6508D1c79755ab8f1ba3883cc38f3',
+        router: '0xDA379C01a484fB9F0875730430a418eB8AAFdca2',
+        standardGateway: '0x9b43912709756DcFd34A64D4362b579928fDcC26',
+        weth: '0x0000000000000000000000000000000000000000',
+        wethGateway: '0x0000000000000000000000000000000000000000'
+      }
+    }
   }
 ]
