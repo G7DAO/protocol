04a95565ed9e2a8aefbb85e88c779e0343cdb41c
Anton Mushnin <anton.mushnin@gmail.com>
2024-07-23 13:36:37 +0300

    getBlockExplorerUrl function

--- webapps/world-builder-dashboard/src/utils/web3utils.ts
+++ webapps/world-builder-dashboard/src/utils/web3utils.ts
@@ -1,3 +1,4 @@
+import { HIGH_NETWORKS, LOW_NETWORKS } from '../../constants'
 import { ethers } from 'ethers'
 
 export const convertToBigNumber = (numberString: string, precision = 18) => {
@@ -5,4 +6,11 @@ export const convertToBigNumber = (numberString: string, precision = 18) => {
   const decimalPartPadded = (decimalPart || '').padEnd(precision, '0')
   const bigNumberString = integerPart + decimalPartPadded
   return ethers.BigNumber.from(bigNumberString)
+}
+
+export const getBlockExplorerUrl = (chainId: number) => {
+  const network = [...LOW_NETWORKS, ...HIGH_NETWORKS].find((n) => n.chainId === chainId)
+  if (network?.blockExplorerUrls) {
+    return network.blockExplorerUrls[0]
+  }
 }
