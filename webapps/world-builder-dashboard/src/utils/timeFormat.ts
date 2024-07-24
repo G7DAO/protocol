a7144d77563ef346dd855b52a1214c2975fb389b
Anton Mushnin <anton.mushnin@gmail.com>
2024-07-24 20:12:16 +0300

    short time format

--- webapps/world-builder-dashboard/src/utils/timeFormat.ts
+++ webapps/world-builder-dashboard/src/utils/timeFormat.ts
@@ -1,4 +1,4 @@
-export const timeAgo = (timestamp: number | undefined) => {
+export const timeAgo = (timestamp: number | undefined, short = true) => {
   if (!timestamp) {
     return ''
   }
@@ -7,18 +7,18 @@ export const timeAgo = (timestamp: number | undefined) => {
   const timeDifference = Math.floor((now - date) / 1000)
 
   const units = [
-    { name: 'year', inSeconds: 60 * 60 * 24 * 365 },
-    { name: 'month', inSeconds: 60 * 60 * 24 * 30 },
-    { name: 'day', inSeconds: 60 * 60 * 24 },
-    { name: 'hour', inSeconds: 60 * 60 },
-    { name: 'minute', inSeconds: 60 },
-    { name: 'second', inSeconds: 1 }
+    { name: 'year', inSeconds: 60 * 60 * 24 * 365, shortName: 'y' },
+    { name: 'month', inSeconds: 60 * 60 * 24 * 30, shortName: 'mo' },
+    { name: 'day', inSeconds: 60 * 60 * 24, shortName: 'd' },
+    { name: 'hour', inSeconds: 60 * 60, shortName: 'h' },
+    { name: 'minute', inSeconds: 60, shortName: 'm' },
+    { name: 'second', inSeconds: 1, shortName: 's' }
   ]
 
   for (const unit of units) {
     const value = Math.floor(timeDifference / unit.inSeconds)
     if (value >= 1) {
-      return `${value} ${unit.name}${value > 1 ? 's' : ''} ago`
+      return `${value}${short ? '' : ' '}${short ? unit.shortName : unit.name}${short ? '' : value > 1 ? 's' : ''} ago`
     }
   }
   return 'just now'
