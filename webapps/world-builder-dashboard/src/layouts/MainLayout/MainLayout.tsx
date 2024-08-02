c9d3b14a23e00f8001aa4552ac2a2a9ceca4ce2f
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-02 10:41:03 +0300

    responsive layout

--- webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
+++ webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
@@ -1,15 +1,14 @@
 // React and related libraries
 import React from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 // Styles
 import styles from './MainLayout.module.css'
 import IconDroplets02 from '@/assets/IconDroplets02'
-import IconGame7 from '@/assets/IconGame7'
-import IconGame7Logo from '@/assets/IconGame7Logo'
-import IconLogout from '@/assets/IconLogout'
 import IconWallet04 from '@/assets/IconWallet04'
 // Local components and assets
-import { useBlockchainContext } from '@/contexts/BlockchainContext'
+import DesktopSidebar from '@/layouts/MainLayout/DesktopSidebar'
+import MobileSidebar from '@/layouts/MainLayout/MobileSidebar'
+import { useMediaQuery } from '@mantine/hooks'
 
 interface MainLayoutProps {}
 
@@ -18,44 +17,10 @@ const NAVIGATION_ITEMS = [
   { name: 'faucet', navigateTo: '/faucet', icon: <IconDroplets02 /> }
 ]
 const MainLayout: React.FC<MainLayoutProps> = ({}) => {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const { connectedAccount, isMetaMask, disconnectWallet } = useBlockchainContext()
-
+  const smallView = useMediaQuery('(max-width: 767px)')
   return (
     <div className={styles.container}>
-      <div className={styles.sideBar}>
-        <div className={styles.sideBarTop}>
-          <div className={styles.logoContainer}>
-            <div className={styles.logoWrapper}>
-              <IconGame7Logo />
-              <IconGame7 />
-            </div>
-          </div>
-          <div className={styles.navigation}>
-            {NAVIGATION_ITEMS.map((item) => (
-              <div
-                className={location.pathname.startsWith(item.navigateTo) ? styles.selectedNavButton : styles.navButton}
-                onClick={() => navigate(item.navigateTo)}
-                key={item.name}
-              >
-                {item.icon}
-                {item.name}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={styles.footer}>
-          {connectedAccount && (
-            <div className={styles.web3AddressContainer}>
-              <div
-                className={styles.web3address}
-              >{`${connectedAccount.slice(0, 6)}...${connectedAccount.slice(-4)}`}</div>
-              {isMetaMask && <IconLogout onClick={disconnectWallet} className={styles.disconnectButton} />}
-            </div>
-          )}
-        </div>
-      </div>
+      {smallView ? <MobileSidebar /> : <DesktopSidebar navigationItems={NAVIGATION_ITEMS} />}
       <Outlet />
     </div>
   )
