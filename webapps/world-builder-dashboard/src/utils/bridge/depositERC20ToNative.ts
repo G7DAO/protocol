7f8a3830ce4a0cd5fa1d0fc904601764ec364178
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-07 12:13:46 +0300

    refactoring ERC20 to Native gas estimation

--- webapps/world-builder-dashboard/src/utils/bridge/depositERC20ToNative.ts
+++ webapps/world-builder-dashboard/src/utils/bridge/depositERC20ToNative.ts
@@ -2,33 +2,12 @@ import { ethers, providers, utils } from 'ethers'
 import { HighNetworkInterface, NetworkInterface } from '@/contexts/BlockchainContext'
 import { TransactionRecord } from '@/utils/bridge/depositERC20ArbitrumSDK'
 import { convertToBigNumber } from '@/utils/web3utils'
+import { ERC20_INBOX_ABI } from '@/web3/ABI/erc20_inbox_abi'
 import { NodeInterface__factory } from '@arbitrum/sdk/dist/lib/abi/factories/NodeInterface__factory'
 import { NODE_INTERFACE_ADDRESS } from '@arbitrum/sdk/dist/lib/dataEntities/constants'
 import { Signer } from '@ethersproject/abstract-signer'
 
-const ERC20_INBOX_ABI = [
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256'
-      }
-    ],
-    name: 'depositERC20',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  }
-]
-
-export const estimateDepositERC20ToNativeFee = async (
+const estimateGasComponents = async (
   amount: string,
   account: string,
   lowNetwork: NetworkInterface,
@@ -38,126 +17,72 @@ export const estimateDepositERC20ToNativeFee = async (
     console.log('inbox contract is undefined')
     return
   }
+
   const destinationAddress = highNetwork.inbox
+  const ethAmount = convertToBigNumber(amount)
   const lowNetworkProvider = new providers.JsonRpcProvider(lowNetwork.rpcs[0])
 
-  const ethAmount = convertToBigNumber(amount)
   const ERC20InboxContract = new ethers.Contract(highNetwork.inbox, ERC20_INBOX_ABI, lowNetworkProvider)
   const tx = await ERC20InboxContract.populateTransaction.depositERC20(ethAmount)
   const data = tx.data
 
-  const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, lowNetworkProvider)
-
   if (data) {
+    const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, lowNetworkProvider)
     try {
-      const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
-        destinationAddress,
-        false,
-        data,
-        {
-          from: account
-        }
-      )
-      // Getting useful values for calculating the formula
-      const l1GasEstimated = gasEstimateComponents.gasEstimateForL1
-      const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1)
-      const l2EstimatedPrice = gasEstimateComponents.baseFee
-      const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16)
-
-      // Calculating some extra values to be able to apply all variables of the formula
-      // -------------------------------------------------------------------------------
-      // NOTE: This one might be a bit confusing, but l1GasEstimated (B in the formula) is calculated based on l2 gas fees
-      const l1Cost = l1GasEstimated.mul(l2EstimatedPrice)
-      // NOTE: This is similar to 140 + utils.hexDataLength(txData);
-      const l1Size = l1Cost.div(l1EstimatedPrice)
-
-      // Getting the result of the formula
-      // ---------------------------------
-      // Setting the basic variables of the formula
-      const P = l2EstimatedPrice
-      const L2G = l2GasUsed
-      const L1P = l1EstimatedPrice
-      const L1S = l1Size
-
-      // L1C (L1 Cost) = L1P * L1S
-      const L1C = L1P.mul(L1S)
-
-      // B (Extra Buffer) = L1C / P
-      const B = L1C.div(P)
-
-      // G (Gas Limit) = L2G + B
-      const G = L2G.add(B)
-
-      // TXFEES (Transaction fees) = P * G
-      const TXFEES = P.mul(G)
-
-      return utils.formatEther(TXFEES)
+      return await nodeInterface.callStatic.gasEstimateComponents(destinationAddress, false, data, {
+        from: account
+      })
     } catch (e: any) {
       console.log("Can't estimate gas: ", e.message)
     }
   }
 }
 
-const estimateDepositERC20ToNativeGas = async (
+const calculateGasValues = (gasEstimateComponents: any) => {
+  const l1GasEstimated = gasEstimateComponents.gasEstimateForL1
+  const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1)
+  const l2EstimatedPrice = gasEstimateComponents.baseFee
+  const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16)
+
+  const l1Cost = l1GasEstimated.mul(l2EstimatedPrice)
+  const l1Size = l1Cost.div(l1EstimatedPrice)
+
+  const P = l2EstimatedPrice
+  const L2G = l2GasUsed
+  const L1P = l1EstimatedPrice
+  const L1S = l1Size
+
+  const L1C = L1P.mul(L1S)
+  const B = L1C.div(P)
+  const G = L2G.add(B)
+
+  return { TXFEES: P.mul(G), gasLimit: G }
+}
+
+export const estimateDepositERC20ToNativeFee = async (
   amount: string,
   account: string,
   lowNetwork: NetworkInterface,
   highNetwork: HighNetworkInterface
 ) => {
-  const destinationAddress = highNetwork.inbox
-
-  const ethAmount = convertToBigNumber(amount)
-  const lowNetworkProvider = new providers.JsonRpcProvider(lowNetwork.rpcs[0])
-
-  const ERC20InboxContract = new ethers.Contract(highNetwork.inbox, ERC20_INBOX_ABI, lowNetworkProvider)
-  const tx = await ERC20InboxContract.populateTransaction.depositERC20(ethAmount)
-  const data = tx.data
-  const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, lowNetworkProvider)
-  if (data) {
-    try {
-      const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
-        destinationAddress,
-        false,
-        data,
-        {
-          from: account
-        }
-      )
-      // Getting useful values for calculating the formula
-      const l1GasEstimated = gasEstimateComponents.gasEstimateForL1
-      const l2GasUsed = gasEstimateComponents.gasEstimate.sub(gasEstimateComponents.gasEstimateForL1)
-      const l2EstimatedPrice = gasEstimateComponents.baseFee
-      const l1EstimatedPrice = gasEstimateComponents.l1BaseFeeEstimate.mul(16)
-
-      // Calculating some extra values to be able to apply all variables of the formula
-      // -------------------------------------------------------------------------------
-      // NOTE: This one might be a bit confusing, but l1GasEstimated (B in the formula) is calculated based on l2 gas fees
-      const l1Cost = l1GasEstimated.mul(l2EstimatedPrice)
-      // NOTE: This is similar to 140 + utils.hexDataLength(txData);
-      const l1Size = l1Cost.div(l1EstimatedPrice)
-
-      // Getting the result of the formula
-      // ---------------------------------
-      // Setting the basic variables of the formula
-      const P = l2EstimatedPrice
-      const L2G = l2GasUsed
-      const L1P = l1EstimatedPrice
-      const L1S = l1Size
-
-      // L1C (L1 Cost) = L1P * L1S
-      const L1C = L1P.mul(L1S)
-
-      // B (Extra Buffer) = L1C / P
-      const B = L1C.div(P)
-
-      // G (Gas Limit) = L2G + B
-      return L2G.add(B)
-    } catch (e: any) {
-      console.log("Can't estimate gas: ", e.message)
-    }
+  const gasEstimateComponents = await estimateGasComponents(amount, account, lowNetwork, highNetwork)
+  if (gasEstimateComponents) {
+    const { TXFEES } = calculateGasValues(gasEstimateComponents)
+    return utils.formatEther(TXFEES)
   }
+}
 
-  // return ethers.BigNumber.from('0x346d32') //fallback for zero price (P);
+export const estimateDepositERC20ToNativeGas = async (
+  amount: string,
+  account: string,
+  lowNetwork: NetworkInterface,
+  highNetwork: HighNetworkInterface
+) => {
+  const gasEstimateComponents = await estimateGasComponents(amount, account, lowNetwork, highNetwork)
+  if (gasEstimateComponents) {
+    const { gasLimit } = calculateGasValues(gasEstimateComponents)
+    return gasLimit
+  }
 }
 
 export const sendDepositERC20ToNativeTransaction = async (
