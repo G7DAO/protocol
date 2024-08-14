dd34dcfbf28fad82f20b496e95680147cb7d0a90
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-14 13:19:17 +0300

    feat: Settings View

--- webapps/world-builder-dashboard/src/pages/BridgePage/BridgePage.tsx
+++ webapps/world-builder-dashboard/src/pages/BridgePage/BridgePage.tsx
@@ -8,6 +8,7 @@ import styles from './BridgePage.module.css'
 import BridgeView from '@/components/bridge/bridge/BridgeView'
 import HistoryDesktop from '@/components/bridge/history/HistoryDesktop'
 import HistoryMobile from '@/components/bridge/history/HistoryMobile'
+import SettingsView from '@/components/bridge/settings/SettingsView'
 import NotificationsButton from '@/components/notifications/NotificationsButton'
 import { FloatingNotification } from '@/components/notifications/NotificationsDropModal'
 // Contexts
@@ -68,11 +69,22 @@ const BridgePage = () => {
           >
             History
           </button>
+          <button
+            className={
+              location.pathname === '/bridge/settings'
+                ? styles.selectedNavigationButton
+                : styles.unselectedNavigationButton
+            }
+            onClick={() => navigate('/bridge/settings')}
+          >
+            Settings
+          </button>
         </div>
       </div>
       <div className={styles.viewContainer}>
         {location.pathname === '/bridge' && <BridgeView direction={direction} setDirection={setDirection} />}
         {location.pathname === '/bridge/transactions' && (!smallView ? <HistoryDesktop /> : <HistoryMobile />)}
+        {location.pathname === '/bridge/settings' && <SettingsView />}
       </div>
     </div>
   )
