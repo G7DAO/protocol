b1998a44a39aecb3a461cfe4f2a076316bcdfb5d
Memo Khoury <majd.khoury93@gmail.com>
2024-08-15 19:41:23 +0100

    remove useEffects

--- webapps/world-builder-dashboard/src/components/stake/pools/EditPoolModal.tsx
+++ webapps/world-builder-dashboard/src/components/stake/pools/EditPoolModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Modal, ModalProps } from 'summon-ui/mantine'; // assuming the path to your modal component
 import styles from './EditPoolModal.module.css'
 import ActionButtonStake from '../ActionButtonStake';
@@ -17,35 +17,9 @@ interface EditPoolModalProps extends ModalProps {
 const EditPoolModal: React.FC<EditPoolModalProps> = ({ opened, onClose, poolData }) => {
     const poolId = poolData.poolId;
     const [transferable, setTransferable] = useState(poolData.transferable)
-    const [cooldownSeconds, setCooldownSeconds] = useState(poolData.cooldownSeconds)
-    const [lockupSeconds, setLockupSeconds] = useState(poolData.lockupSeconds)
-    const [changeTransferability, setChangeTransferability] = useState(false)
-    const [changeLockup, setChangeLockup] = useState(false)
-    const [changeCooldown, setChangeCooldown] = useState(false)
+    const [cooldownSeconds, setCooldownSeconds] = useState(Number(poolData.cooldownSeconds))
+    const [lockupSeconds, setLockupSeconds] = useState(Number(poolData.lockupSeconds))
     const [networkErrorMessage, setNetworkErrorMessage] = useState('')
-
-    // To save on gas in the function, we only update the values in smart contract if the 
-    // values from frontend change. See `Staker.sol` lines 203-211 for further clarification
-    useEffect(() => {
-        if (lockupSeconds != poolData.lockupSeconds)
-            setChangeLockup(true)
-        else
-            setChangeLockup(false)
-    }, [lockupSeconds])
-
-    useEffect(() => {
-        if (cooldownSeconds != poolData.cooldownSeconds)
-            setChangeCooldown(true)
-        else
-            setChangeCooldown(false)
-    }, [cooldownSeconds])
-
-    useEffect(() => {
-        if (transferable !== poolData.transferable)
-            setChangeTransferability(true);
-        else
-            setChangeTransferability(false);
-    }, [transferable])
 
     return (
         <Modal opened={opened} onClose={onClose} onClick={(e) => { e.stopPropagation() }}>
@@ -71,7 +45,7 @@ const EditPoolModal: React.FC<EditPoolModalProps> = ({ opened, onClose, poolData
                         type="number"
                         className={styles.input}
                         value={cooldownSeconds}
-                        onChange={(e) => setCooldownSeconds(e.target.value)}
+                        onChange={(e) => setCooldownSeconds(Number(e.target.value))}
                     />
                 </div>
                 <div className={styles.inputGroup}>
@@ -80,14 +54,22 @@ const EditPoolModal: React.FC<EditPoolModalProps> = ({ opened, onClose, poolData
                         type="number"
                         className={styles.input}
                         value={lockupSeconds}
-                        onChange={(e) => setLockupSeconds(e.target.value)}
+                        onChange={(e) => setLockupSeconds(Number(e.target.value))}
                     />
                 </div>
                 {networkErrorMessage && <div className={styles.networkErrorMessage}>{networkErrorMessage}</div>}
                 <ActionButtonStake
                     actionType={"EDITPOOL"}
-                    params={{ poolId, changeTransferability, transferable, changeLockup, lockupSeconds, changeCooldown, cooldownSeconds }}
-                    isDisabled={!changeLockup && !changeCooldown && !changeTransferability}
+                    params={{ 
+                        poolId, 
+                        changeTransferability: transferable !== poolData.transferable, 
+                        transferable, 
+                        changeLockup: lockupSeconds !== Number(poolData.lockupSeconds), 
+                        lockupSeconds: lockupSeconds.toString(), 
+                        changeCooldown: cooldownSeconds !== Number(poolData.cooldownSeconds), 
+                        cooldownSeconds: cooldownSeconds.toString(),
+                    }}
+                    isDisabled={transferable === poolData.transferable && lockupSeconds === Number(poolData.lockupSeconds) && cooldownSeconds === Number(poolData.cooldownSeconds)}
                     setErrorMessage={setNetworkErrorMessage}
                 />
             </div>
