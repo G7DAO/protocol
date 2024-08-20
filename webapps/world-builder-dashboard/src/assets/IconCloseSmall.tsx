3af3f230d5fea95be4048fd220e03bc706ed2463
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-20 13:13:27 +0300

    feat: close button on floating notifications

---
+++ webapps/world-builder-dashboard/src/assets/IconCloseSmall.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IconCloseSmall: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20' fill='none' {...props}>
+    <path d='M15 5L5 15M5 5L15 15' strokeWidth='1.66667' strokeLinecap='round' strokeLinejoin='round' />
+  </svg>
+)
+
+export default IconCloseSmall
