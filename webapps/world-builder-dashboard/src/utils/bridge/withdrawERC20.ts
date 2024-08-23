eaac4464059e1d52535de5c740257195fae9c211
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-23 17:14:00 +0300

    refactor: move L2GatewayRouterABI and estimateOutboundTransferGas to separate files

--- webapps/world-builder-dashboard/src/utils/bridge/withdrawERC20.ts
+++ webapps/world-builder-dashboard/src/utils/bridge/withdrawERC20.ts
@@ -1,43 +1,7 @@
 import { L1_NETWORK, L2_NETWORK } from '../../../constants'
 import { ethers } from 'ethers'
 import { TransactionRecord } from '@/utils/bridge/depositERC20ArbitrumSDK'
-
-const L2GatewayRouterABI = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_l1Token',
-        type: 'address'
-      },
-      {
-        internalType: 'address',
-        name: '_to',
-        type: 'address'
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256'
-      },
-      {
-        internalType: 'bytes',
-        name: '_data',
-        type: 'bytes'
-      }
-    ],
-    name: 'outboundTransfer',
-    outputs: [
-      {
-        internalType: 'bytes',
-        name: '',
-        type: 'bytes'
-      }
-    ],
-    stateMutability: 'payable',
-    type: 'function'
-  }
-]
+import { L2GatewayRouterABI } from '@/web3/ABI/l2GatewayRouter_abi'
 
 const L2GatewayRouterAddress = '0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7'
 export const sendWithdrawERC20Transaction = async (
@@ -78,30 +42,6 @@ export const sendWithdrawERC20Transaction = async (
     }
   } catch (error) {
     console.error('Transaction failed:', error)
-    throw error
-  }
-}
-
-export const estimateOutboundTransferGas = async (
-  contractAddress: string,
-  _l1Token: string,
-  _to: string,
-  _amount: ethers.BigNumberish,
-  _data: string | ethers.BytesLike,
-  provider: ethers.providers.Provider
-) => {
-  // Create a contract instance
-  const contract = new ethers.Contract(contractAddress, L2GatewayRouterABI, provider)
-
-  // Estimate gas
-  try {
-    const estimatedGas = await contract.estimateGas.outboundTransfer(_l1Token, _to, _amount, _data, {
-      value: ethers.utils.parseEther('0') // Adjust if the function requires ETH
-    })
-    console.log(`Estimated Gas: ${estimatedGas.toString()}`)
-    return estimatedGas
-  } catch (error) {
-    console.error('Error estimating gas:', error)
     throw error
   }
 }
