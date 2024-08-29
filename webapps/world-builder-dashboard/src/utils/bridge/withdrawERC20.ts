3abb8e9ec383866f2aab1a604317233a7988c5a9
Anton Mushnin <anton.mushnin@gmail.com>
2024-08-29 16:03:27 +0300

    refactor: update sendWithdrawERC20Transaction to accept signer parameter

--- webapps/world-builder-dashboard/src/utils/bridge/withdrawERC20.ts
+++ webapps/world-builder-dashboard/src/utils/bridge/withdrawERC20.ts
@@ -5,35 +5,28 @@ import { L2GatewayRouterABI } from '@/web3/ABI/l2GatewayRouter_abi'
 
 const L2GatewayRouterAddress = '0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7'
 export const sendWithdrawERC20Transaction = async (
-  amountInNative: string,
-  destination: string
+  value: string,
+  destination: string,
+  signer: ethers.Signer
 ): Promise<TransactionRecord> => {
   try {
-    if (!window.ethereum) {
-      throw new Error('no provider')
-    }
-    const amountInWei = ethers.utils.parseEther(amountInNative.toString())
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
-    const signer = provider.getSigner()
-
+    const valueInWei = ethers.utils.parseEther(value)
     const routerContract = new ethers.Contract(L2GatewayRouterAddress, L2GatewayRouterABI, signer)
 
     const txRequest = await routerContract.populateTransaction.outboundTransfer(
       L1_NETWORK.g7TokenAddress,
       destination,
-      amountInWei,
+      valueInWei,
       '0x'
     )
 
     const txResponse = await signer.sendTransaction(txRequest)
 
-    // Wait for the transaction to be mined
     await txResponse.wait()
 
     return {
       type: 'WITHDRAWAL',
-      amount: amountInNative,
+      amount: value,
       lowNetworkChainId: L1_NETWORK.chainId,
       highNetworkChainId: L2_NETWORK.chainId,
       highNetworkHash: txResponse.hash,
