e7f67664a7a18244b902df47e6000a9254c76009
Anton Mushnin <anton.mushnin@gmail.com>
2024-09-09 11:04:20 +0300

    Bridge SDK example - types: extend Ethereum provider interface and add global window definition

---
+++ webapps/game7-bridge-example/src/global.d.ts
@@ -0,0 +1,14 @@
+import { ExternalProvider } from '@ethersproject/providers'
+
+export interface Ethereumish extends ExternalProvider {
+  request: (args: { method: string; params?: Array<any> }) => Promise<any>
+  isMetaMask?: boolean
+  on?: (event: string, listener: (...args: any[]) => void) => void
+  removeListener?: (event: string, listener: (...args: any[]) => void) => void
+}
+
+declare global {
+  interface Window {
+    ethereum?: Ethereumish
+  }
+}
