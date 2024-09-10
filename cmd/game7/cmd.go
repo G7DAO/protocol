28c2642fb1d49b32280c4cde0d79cbe520fb2c55
Daniel Lima <daniel.lima@orium.network>
2024-09-10 15:37:32 -0300

    Feat: Add Token Sender CLI and ABI

--- cmd/game7/cmd.go
+++ cmd/game7/cmd.go
@@ -15,6 +15,7 @@ import (
 	"github.com/G7DAO/protocol/bindings/PositionMetadata"
 	"github.com/G7DAO/protocol/bindings/Staker"
 	"github.com/G7DAO/protocol/bindings/TokenFaucet"
+	"github.com/G7DAO/protocol/bindings/TokenSender"
 	"github.com/G7DAO/protocol/bridge"
 
 	"github.com/G7DAO/protocol/bindings/ERC20"
@@ -65,6 +66,9 @@ func CreateRootCommand() *cobra.Command {
 	faucetCmd := TokenFaucet.CreateTokenFaucetCommand()
 	faucetCmd.Use = "faucet"
 
+	tokenSenderCmd := TokenSender.CreateTokenSenderCommand()
+	tokenSenderCmd.Use = "token-sender"
+
 	accountsCmd := accounts.CreateAccountsCommand()
 	accountsCmd.Use = "accounts"
 
@@ -79,7 +83,7 @@ func CreateRootCommand() *cobra.Command {
 
 	mockCmd := CreateMockCommand()
 
-	rootCmd.AddCommand(completionCmd, versionCmd, tokenCmd, arbitrumL1OrbitCustomGatewayCmd, arbitrumL2CustomGatewayCmd, arbitrumUpgradeExecutorCmd, arbitrumL1OrbitGatewayRouterCmd, arbSysCmd, erc20InboxCmd, bridgeCmd, faucetCmd, accountsCmd, wrappedNativeTokenCmd, stakerCmd, mockCmd, positionMetadataCmd)
+	rootCmd.AddCommand(completionCmd, versionCmd, tokenCmd, arbitrumL1OrbitCustomGatewayCmd, arbitrumL2CustomGatewayCmd, arbitrumUpgradeExecutorCmd, arbitrumL1OrbitGatewayRouterCmd, arbSysCmd, erc20InboxCmd, bridgeCmd, faucetCmd, accountsCmd, wrappedNativeTokenCmd, stakerCmd, mockCmd, positionMetadataCmd, tokenSenderCmd)
 
 	// By default, cobra Command objects write to stderr. We have to forcibly set them to output to
 	// stdout.
