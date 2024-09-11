5974e6b2bd3c68c127e9d4c9d1dbbade65baa548
Daniel Lima <daniel.lima@orium.network>
2024-09-11 19:42:14 -0300

    Feat: Update KMS credentials

--- apis/protocol-api/src/config.ts
+++ apis/protocol-api/src/config.ts
@@ -9,7 +9,7 @@ export const GAME7_TESTNET_RPC_URL = process.env.GAME7_TESTNET_RPC_URL || 'https
 export const KMS_CREDENTIALS = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAxxxxxxxxxxxxxxxx', // credentials for your IAM user with KMS access
     secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // credentials for your IAM user with KMS access
-    region: 'us-east-1', // region of your KMS key
+    region: process.env.AWS_REGION || 'us-east-1', // region of your KMS key
     keyId: process.env.AWS_KMS_KEY_ID || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', // KMS key id (arn)
   }
 export const TOKEN_SENDER_AMOUNT = process.env.TOKEN_SENDER_AMOUNT || ethers.parseEther('1')
--- apis/protocol-api/src/services/faucet.service.ts
+++ apis/protocol-api/src/services/faucet.service.ts
@@ -1,13 +1,29 @@
 import { ethers } from 'ethers';
 import { TokenSenderABI } from '../abis/TokenSenderABI';
-import { GAME7_TESTNET_RPC_URL, KMS_CREDENTIALS, TOKEN_SENDER_ADDRESS, TOKEN_SENDER_AMOUNT } from '../config';
+import {
+  GAME7_TESTNET_RPC_URL,
+  KMS_CREDENTIALS,
+  TOKEN_SENDER_ADDRESS,
+  TOKEN_SENDER_AMOUNT,
+} from '../config';
 import { AwsKmsSigner } from '../utils/ethers-aws-kms-signer';
 
 export class FaucetService {
   tokenSender: ethers.Contract;
+
   constructor() {
     const provider = new ethers.JsonRpcProvider(GAME7_TESTNET_RPC_URL);
-    const signer = new AwsKmsSigner(KMS_CREDENTIALS).connect(provider)
+    const signer = new AwsKmsSigner(
+      {
+        region: KMS_CREDENTIALS.region,
+        keyId: KMS_CREDENTIALS.keyId,
+        credentials: {
+          accessKeyId: KMS_CREDENTIALS.accessKeyId,
+          secretAccessKey: KMS_CREDENTIALS.secretAccessKey,
+        },
+      },
+      provider
+    );
     this.tokenSender = new ethers.Contract(
       TOKEN_SENDER_ADDRESS,
       TokenSenderABI,
@@ -16,8 +32,10 @@ export class FaucetService {
   }
 
   async requestFaucet(recipientAddress: string) {
-      const tx = await this.tokenSender.send(recipientAddress, { value: TOKEN_SENDER_AMOUNT });
-      await tx.wait();
-      return tx.hash
+    const tx = await this.tokenSender.send(recipientAddress, {
+      value: TOKEN_SENDER_AMOUNT,
+    });
+    await tx.wait();
+    return tx.hash;
   }
 }
