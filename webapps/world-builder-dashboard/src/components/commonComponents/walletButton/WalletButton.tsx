455b0c1d7a06a654e1583ec1d6764d732a9c29ad
Memo Khoury <majd.khoury93@gmail.com>
2024-09-18 15:47:16 +0100

    tokens

--- webapps/world-builder-dashboard/src/components/commonComponents/walletButton/WalletButton.tsx
+++ webapps/world-builder-dashboard/src/components/commonComponents/walletButton/WalletButton.tsx
@@ -7,6 +7,8 @@ import useNativeBalance from '@/hooks/useNativeBalance'
 import { useBlockchainContext } from '@/contexts/BlockchainContext'
 import { L3_NETWORK } from '../../../../constants'
 import { roundToDecimalPlaces } from '@/utils/web3utils'
+import IconUSDC from '@/assets/IconUSDC'
+import IconG7T from '@/assets/IconG7T'
 
 interface WalletButtonProps { }
 
@@ -39,9 +41,50 @@ const WalletButton: React.FC<WalletButtonProps> = () => {
             </div>
             <Modal opened={isModalOpen} onClose={handleModalClose}
                 radius={'12px'}
+                padding={'24px'}
                 size={'678px'}
                 title={'Wallet Balance'}
-            />
+                classNames={{ header: styles.header, title: styles.title }}
+                shadow='box-shadow: 0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03);'
+            >
+                <div className={styles.modalContent}>
+                    <div className={styles.tokensContainer}>
+                        <div className={styles.tokenRow}>
+                            <div className={styles.tokenInformation}>
+                                <div className={styles.tokenIconContainer}>
+                                    <IconUSDC className={styles.token} />
+                                </div>
+                                <div className={styles.tokenTextContainer}>
+                                    <div className={styles.tokenTitle}>
+                                        USDC
+                                    </div>
+                                    <div className={styles.tokenSymbol}>
+                                        USDC
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.tokenRow}>
+                            <div className={styles.tokenInformation}>
+                                <div className={styles.tokenIconContainer}>
+                                    <IconG7T className={styles.token} />
+                                </div>
+                                <div className={styles.tokenTextContainer}>
+                                    <div className={styles.tokenTitle}>
+                                        G7DAO
+                                    </div>
+                                    <div className={styles.tokenSymbol}>
+                                        G7T
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.balanceText}>
+                                1.2
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </>
     )
 }
