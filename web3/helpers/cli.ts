2997c9ef3907bdbb8a6cfbbdb1b1e49bca40009b
Daniel Lima <djlblockchain@gmail.com>
2024-09-18 23:10:05 -0300

    Feat: Deploy contract with safe working

--- web3/helpers/cli.ts
+++
@@ -1,7 +0,0 @@
-export function getArgFromCLI(arg: string): string | undefined {
-    const index = process.argv.indexOf(arg);
-    if (index !== -1 && index < process.argv.length - 1) {
-        return process.argv[index + 1];
-    }
-    return undefined;
-}
--- web3/helpers/safe.ts
+++ web3/helpers/safe.ts
@@ -1,20 +1,64 @@
-export const Networks = {
-    SEPOLIA: "sepolia",
-    ARBITRUM_SEPOLIA: "arbitrum-sepolia",
-    BASE_SEPOLIA: "base-sepolia",
-    GAME7_TESTNET: "game7-testnet",
-}
+import SafeApiKit from '@safe-global/api-kit';
+import Safe, { SafeConfigWithSafeAddress } from '@safe-global/protocol-kit';
+import { MetaTransactionData } from '@safe-global/safe-core-sdk-types';
+import { ethers } from 'ethers';
+import { TxServiceUrl } from './type';
 
-export const ChainIds = {
-    [Networks.SEPOLIA]: 1,
-    [Networks.ARBITRUM_SEPOLIA]: 421611,
-    [Networks.BASE_SEPOLIA]: 84532,
-    [Networks.GAME7_TESTNET]: 13746,
-}
+/**
+ * Create a Safe proposal
+ * @param rpcUrl - The RPC URL
+ * @param wallet - The wallet
+ * @param safeAddress - The Safe address
+ * @param to - The address to send the transaction to
+ * @param data - The calldata
+ * @param value - The value to send
+ */
+export async function createSafeProposal(
+    rpcUrl: string,
+    wallet: ethers.Wallet | ethers.HDNodeWallet,
+    safeAddress: string,
+    to: string,
+    data: string,
+    value: string
+) {
+    const config: SafeConfigWithSafeAddress = {
+        provider: rpcUrl,
+        signer: wallet.privateKey,
+        isL1SafeSingleton: true,
+        safeAddress,
+    };
 
-export const TxServiceUrl = {
-    [ChainIds[Networks.SEPOLIA]]: "https://safe-transaction-sepolia.safe.global",
-    [ChainIds[Networks.ARBITRUM_SEPOLIA]]: "https://safe-transaction-arbitrum-sepolia.safe.global",
-    [ChainIds[Networks.BASE_SEPOLIA]]: "https://safe-transaction-base-sepolia.safe.global",
-    [ChainIds[Networks.GAME7_TESTNET]]: "https://safe-transaction-game7-testnet.safe.global",
+    const safe = await Safe.init(config);
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+    const chainId = (await provider.getNetwork()).chainId;
+
+    const safeTransactionData: MetaTransactionData = {
+        to,
+        data,
+        value,
+    };
+
+    // Create a Safe transaction with the provided parameters
+    const safeTransaction = await safe.createTransaction({ transactions: [safeTransactionData] });
+
+    // Deterministic hash based on transaction parameters
+    const safeTxHash = await safe.getTransactionHash(safeTransaction);
+
+    // Sign transaction to verify that the transaction is coming from owner 1
+    const senderSignature = await safe.signHash(safeTxHash);
+
+    const apiKit = new SafeApiKit({
+        chainId,
+        txServiceUrl: TxServiceUrl[Number(chainId)],
+    });
+
+    console.log('Proposing transaction...');
+    await apiKit.proposeTransaction({
+        safeAddress,
+        safeTransactionData: safeTransaction.data,
+        safeTxHash,
+        senderAddress: wallet.address,
+        senderSignature: senderSignature.data,
+    });
+    console.log('Proposal submitted');
 }
