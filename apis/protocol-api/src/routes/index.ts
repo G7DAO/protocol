196ce15a4c5348f0bc2823896cc4d936550d9d3d
Daniel Lima <djlblockchain@gmail.com>
2024-09-24 14:19:42 -0300

    Feat: Add Cors

--- apis/protocol-api/src/routes/index.ts
+++ apis/protocol-api/src/routes/index.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express';
 
 import faucetRouter from './faucet.router';
+import cors from 'cors';
 
 const setupRouter = (): Router => {
   const router = Router();
+  // CORS configuration
+  const allowedOrigins = process.env.PROTOCOL_API_CORS?.split(',') || [];
+  const corsOptions: cors.CorsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+
+  // Apply CORS middleware to all routes
+  router.use(cors(corsOptions));
+
   router.use('/faucet', faucetRouter());
 
   return router;
