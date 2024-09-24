55b620c518d343e0247e886c9aa7c72974e4e31f
Anton Mushnin <anton.mushnin@gmail.com>
2024-09-24 12:58:37 +0300

    feat: implement gas and fee estimation for outbound transfer

--- webapps/packages/game7-bridge-sdk/src/actions/deposit.ts
+++ webapps/packages/game7-bridge-sdk/src/actions/deposit.ts
@@ -1,9 +1,11 @@
 import { BigNumber, ethers, PayableOverrides } from 'ethers';
-import { getArbitrumNetwork } from '@arbitrum/sdk';
-import { BridgerError, UnsupportedNetworkError } from '../errors';
+import { Erc20Bridger, getArbitrumNetwork } from '@arbitrum/sdk';
+import { UnsupportedNetworkError } from '../errors';
 import { ERC20_INBOX_ABI } from '../abi/erc20_inbox_abi';
 import { BridgeNetworkConfig } from '../networks';
-import { L1GatewayRouterABI } from '../abi/L1GatewayRouterABI';
+import { Erc20DepositParams } from '@arbitrum/sdk/dist/lib/assetBridger/erc20Bridger';
+import { L2GatewayRouterABI } from '../abi/L2GatewayRouterABI';
+import { GasAndFeeEstimation } from '../types';
 
 export const depositERC20 = async (
   amount: BigNumber,
@@ -18,48 +20,14 @@ export const depositERC20 = async (
   if (!destinationNetwork) {
     throw new UnsupportedNetworkError(destinationNetworkChainId);
   }
-  // const erc20Bridger = new Erc20Bridger(destinationNetwork);
-  //
-  // const params: Erc20DepositParams = {
-  //   amount,
-  //   erc20ParentAddress: originTokenAddress,
-  //   childProvider,
-  //   parentSigner: signer,
-  //   // overrides,
-  // };
-  // console.log(params);
-  // return erc20Bridger.deposit(params);
-
-  const gatewayRouter = destinationNetwork.tokenBridge?.parentGatewayRouter;
-  if (!gatewayRouter) {
-    throw new BridgerError(
-      `Can't deposit ERC20 without parentGatewayRouter on network #${destinationNetworkChainId}`,
-    );
-  }
-
-  try {
-    const maxGas = ethers.BigNumber.from('79117');
-    const gasPriceBid = ethers.BigNumber.from('887220000');
-    const data = '0x';
-    const gatewayContract = new ethers.Contract(gatewayRouter, L1GatewayRouterABI, signer);
-    const txRequest = await gatewayContract.populateTransaction.outboundTransfer(
-      originTokenAddress,
-      destinationAddress,
-      amount,
-      maxGas,
-      gasPriceBid,
-      data,
-      { value: ethers.utils.parseEther('0') },
-    );
-
-    const txResponse = await signer.sendTransaction(txRequest);
-    await txResponse.wait();
-    console.log(txResponse);
-    return txResponse;
-  } catch (error: any) {
-    console.error('ERC20 deposit transaction failed:', error);
-    throw new BridgerError(`ERC20 deposit failed: ${error.message}`);
-  }
+  const erc20Bridger = new Erc20Bridger(destinationNetwork);
+  const params: Erc20DepositParams = {
+    amount,
+    erc20ParentAddress: originTokenAddress,
+    childProvider,
+    parentSigner: signer,
+  };
+  return await erc20Bridger.deposit(params);
 };
 
 export const depositNative = async (
@@ -68,19 +36,11 @@ export const depositNative = async (
   signer: ethers.Signer,
   overrides?: PayableOverrides,
 ): Promise<ethers.ContractTransaction> => {
-  // const destinationNetwork = getArbitrumNetwork(destinationNetworkChainId);
-
   const destinationAddress = destinationNetwork.ethBridge?.inbox;
   if (!destinationAddress) {
     throw new UnsupportedNetworkError(destinationNetwork.chainId);
   }
   const ERC20InboxContract = new ethers.Contract(destinationAddress, ERC20_INBOX_ABI, signer);
-  // const gasEstimate = await estimateDepositERC20ToNativeGas(
-  //   amount,
-  //   account,
-  //   lowNetwork,
-  //   highNetwork,
-  // );
 
   const txRequest = await ERC20InboxContract.populateTransaction.depositERC20(amount);
 
@@ -89,4 +49,49 @@ export const depositNative = async (
   // Wait for the transaction to be mined
   await txResponse.wait();
   return txResponse;
+};
+
+export const estimateOutboundTransferGas = async (
+  contractAddress: string,
+  _l1Token: string,
+  _to: string,
+  _amount: ethers.BigNumberish,
+  _data: string | ethers.BytesLike,
+  provider: ethers.providers.Provider,
+): Promise<GasAndFeeEstimation> => {
+  const contract = new ethers.Contract(
+    '0x9ed191DB1829371F116Deb9748c26B49467a592A',
+    L2GatewayRouterABI,
+    provider,
+  );
+  try {
+    const estimatedGas = await contract.estimateGas.outboundTransfer(
+      _l1Token,
+      _to,
+      _amount,
+      _data,
+      {
+        value: ethers.utils.parseEther('0.0005'),
+        from: _to,
+      },
+    );
+    const multiplier = ethers.BigNumber.from('1');
+    const gasLimit = estimatedGas.mul(multiplier);
+    const gasPrice = await provider.getGasPrice();
+    const fee = gasLimit.mul(gasPrice);
+    console.log({
+      estimatedGas: ethers.utils.formatUnits(estimatedGas, 18),
+      gasLimit: ethers.utils.formatUnits(gasLimit, 18),
+      gasPrice: ethers.utils.formatUnits(gasPrice, 18),
+      fee: ethers.utils.formatEther(fee),
+    });
+    return {
+      estimatedGas,
+      gasPrice,
+      estimatedFee: fee,
+    };
+  } catch (error) {
+    console.error('Error estimating gas:', error);
+    throw error;
+  }
 };
