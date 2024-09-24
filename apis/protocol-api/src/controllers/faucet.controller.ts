31d3491d8963cf47b3f25c1c27b3207ff0c652ac
Daniel Lima <djlblockchain@gmail.com>
2024-09-24 13:45:47 -0300

    Refactor: from logger to console.logs

--- apis/protocol-api/src/controllers/faucet.controller.ts
+++ apis/protocol-api/src/controllers/faucet.controller.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express';
-import logger from '../utils/logger';
-
 import { FaucetService } from '../services';
 
 export class FaucetController {
@@ -12,12 +10,12 @@ export class FaucetController {
   async sendTokens(req: Request, res: Response) {
     try {
       const { recipientAddress } = req.params;
-      logger.info(`[FaucetController::sendTokens] Sending tokens to ${recipientAddress}`);
+      console.log(`[FaucetController::sendTokens] Sending tokens to ${recipientAddress}`);
       const response = await this.faucetService.send(recipientAddress);
-      logger.info(`[FaucetController::sendTokens] Tokens sent to ${recipientAddress}: ${response}`);
+      console.log(`[FaucetController::sendTokens] Tokens sent to ${recipientAddress}: ${response}`);
       return res.status(200).send({ status: 'success', result: response });
     } catch (error) {
-      logger.error(`[FaucetController::sendTokens] Error sending tokens: ${error}`);
+      console.log(`[FaucetController::sendTokens] Error sending tokens: ${error}`);
       return res.status(500).send({ status: 'error', result: "Internal server error" });
     }
   }
@@ -25,24 +23,24 @@ export class FaucetController {
   async getLastSentTimestamp(req: Request, res: Response) {
     try {
       const { recipientAddress } = req.params;
-      logger.info(`[FaucetController::getLastSentTimestamp] Retrieving last sent timestamp for ${recipientAddress}`);
+      console.log(`[FaucetController::getLastSentTimestamp] Retrieving last sent timestamp for ${recipientAddress}`);
       const response = await this.faucetService.lastSentTimestamp(recipientAddress);
-      logger.info(`[FaucetController::getLastSentTimestamp] Last sent timestamp retrieved for ${recipientAddress}: ${response}`);
+      console.log(`[FaucetController::getLastSentTimestamp] Last sent timestamp retrieved for ${recipientAddress}: ${response}`);
       return res.status(200).send({ status: 'success', result: response });
     } catch (error) {
-      logger.error(`[FaucetController::getLastSentTimestamp] Error retrieving last sent timestamp: ${error}`);
+      console.log(`[FaucetController::getLastSentTimestamp] Error retrieving last sent timestamp: ${error}`);
       return res.status(500).send({ status: 'error', result: "Internal server error" });
     }
   }
 
   async getInterval(req: Request, res: Response) {
     try {
-      logger.info('[FaucetController::getInterval] Retrieving interval');
+      console.log('[FaucetController::getInterval] Retrieving interval');
       const response = await this.faucetService.getInterval();
-      logger.info(`[FaucetController::getInterval] Interval retrieved: ${response}`);
+      console.log(`[FaucetController::getInterval] Interval retrieved: ${response}`);
       return res.status(200).send({ status: 'success', result: response });
     } catch (error) {
-      logger.error(`[FaucetController::getInterval] Error retrieving interval: ${error}`);
+      console.log(`[FaucetController::getInterval] Error retrieving interval: ${error}`);
       return res.status(500).send({ status: 'error', result: "Internal server error" });
     }
   }
@@ -50,12 +48,12 @@ export class FaucetController {
   async getRemainingTime(req: Request, res: Response) {
     try {
       const { recipientAddress } = req.params;
-      logger.info(`[FaucetController::getRemainingTime] Retrieving remaining time for ${recipientAddress}`);
+      console.log(`[FaucetController::getRemainingTime] Retrieving remaining time for ${recipientAddress}`);
       const response = await this.faucetService.getRemainingTime(recipientAddress);
-      logger.info(`[FaucetController::getRemainingTime] Remaining time retrieved for ${recipientAddress}: ${response}`);
+      console.log(`[FaucetController::getRemainingTime] Remaining time retrieved for ${recipientAddress}: ${response}`);
       return res.status(200).send({ status: 'success', result: response });
     } catch (error) {
-      logger.error(`[FaucetController::getRemainingTime] Error retrieving remaining time: ${error}`);
+      console.log(`[FaucetController::getRemainingTime] Error retrieving remaining time: ${error}`);
       return res.status(500).send({ status: 'error', result: "Internal server error" });
     }
   }
