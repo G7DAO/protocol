06f7fcb7a69ab754064b00fa7fd18ed20b23c12e
Daniel Lima <djlblockchain@gmail.com>
2024-09-24 14:42:04 -0300

    PR fixes

---
+++ apis/protocol-api/src/middlewares/cors.middleware.ts
@@ -0,0 +1,23 @@
+import cors from 'cors';
+
+// Parse allowed origins
+const allowedOriginsStr = process.env.PROTOCOL_API_CORS || '';
+const allowedOriginsArray = allowedOriginsStr.split(',').filter(Boolean);
+
+// Set allowedOrigins to "*" if no origins are specified, otherwise create a hash table
+const allowedOrigins: { [key: string]: boolean } = allowedOriginsArray.reduce((acc, origin) => ({ ...acc, [origin]: true }), {});
+
+// CORS configuration
+export const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOriginsArray.length === 0) {
+      callback(null, "*");
+    } else if (allowedOrigins[origin]) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+export const corsMiddleware = cors(corsOptions);
--- apis/protocol-api/src/routes/index.ts
+++ apis/protocol-api/src/routes/index.ts
@@ -1,25 +1,13 @@
 import { Router } from 'express';
-
 import faucetRouter from './faucet.router';
-import cors from 'cors';
+import { corsMiddleware } from '../middlewares/cors.middleware';
 
 const setupRouter = (): Router => {
   const router = Router();
-  // CORS configuration
-  const allowedOrigins = process.env.PROTOCOL_API_CORS?.split(',') || [];
-  const corsOptions: cors.CorsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
 
-  // Apply CORS middleware to all routes
-  router.use(cors(corsOptions));
+  router.use(corsMiddleware);
 
+  // Mount other routers
   router.use('/faucet', faucetRouter());
 
   return router;
