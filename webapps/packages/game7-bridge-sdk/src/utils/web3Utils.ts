f0eb1db3243f3e489a0d31051ad9af18bcfdbf61
Anton Mushnin <anton.mushnin@gmail.com>
2024-09-30 10:55:58 +0300

    feat: add getBlockTimeDifference to calculate time difference between blocks using average block time

---
+++ webapps/packages/game7-bridge-sdk/src/utils/web3Utils.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'ethers';
+
+export const getBlockTimeDifference = async (
+  blockNumber: ethers.BigNumber,
+  provider: ethers.providers.Provider,
+): Promise<number> => {
+  const targetBlockNumber = blockNumber.toNumber();
+
+  // Get the current block number
+  const currentBlock = await provider.getBlockNumber();
+
+  if (targetBlockNumber === currentBlock) {
+    return 0;
+  }
+
+  // Get the current block timestamp and the block from 100 blocks ago for the average block time calculation
+  const currentBlockData = await provider.getBlock(currentBlock);
+  const pastBlockData = await provider.getBlock(currentBlock - 100);
+
+  if (!currentBlockData || !pastBlockData) {
+    throw new Error('Unable to fetch block data');
+  }
+
+  // Calculate the average block time
+  const blockTimeInterval = (currentBlockData.timestamp - pastBlockData.timestamp) / 100;
+
+  const blockDifference = targetBlockNumber - currentBlock;
+  return blockDifference * blockTimeInterval * 1000;
+};
