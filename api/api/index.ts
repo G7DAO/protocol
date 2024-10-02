28b64465a5f39d59cac189c4e3afc6a079f606d9
Josh D <lightspeed186@gmail.com>
2024-10-02 12:25:16 -0400

    add bridge and staker api queries

--- api/api/index.ts
+++ api/api/index.ts
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import {
-    getTransactionHistory
+    getTransactionHistory,
+    getPoolDetails,
+    getAllPoolPositions
 } from "./actions";
 
 const app: Application = express();
@@ -28,6 +30,27 @@ app.get("/history/:address", async (req: Request, res: Response) => {
     }
 });
 
+// Get general staking details on game7 testnet for a poolId
+app.get("/pool/:poolId", async (req: Request, res: Response) => {
+    console.log("req.params.poolId", req.params.poolId);
+    let poolDetails: object | string = await getPoolDetails(req.params.poolId);
+    if (typeof poolDetails == "string") {
+        return res.status(500).send(poolDetails);
+    } else {
+        return res.status(200).json(poolDetails);
+    }
+});
+
+// Get all positions for a poolId on game7 testnet
+app.get("/pool/:poolId/positions", async (req: Request, res: Response) => {
+    console.log("req.params.poolId", req.params.poolId);
+    let positions: object | string = await getAllPoolPositions(req.params.poolId);
+    if (typeof positions == "string") {
+        return res.status(500).send(positions);
+    } else {
+        return res.status(200).json(positions);
+    }
+});
 
 app.listen(PORT, (): void => {
     console.log("SERVER IS UP ON PORT:", PORT);
