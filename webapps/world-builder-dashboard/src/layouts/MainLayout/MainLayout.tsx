34a68d21b8b89c00b6c0d2c7c637c89dd111e090
Memo Khoury <majd.khoury93@gmail.com>
2024-10-09 10:03:56 +0100

    update documentation link

--- webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
+++ webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
@@ -20,7 +20,7 @@ const NAVIGATION_ITEMS = [
   { name: 'bridge', navigateTo: '/bridge', icon: <IconWallet04 /> },
   { name: 'faucet', navigateTo: '/faucet', icon: <IconDroplets02 /> },
   { name: 'explorer', navigateTo: 'https://testnet.game7.io/', icon: <IconExplorer /> },
-  { name: 'documentation', navigateTo: 'https://wiki.game7.io/developer-documents-external/bWmdEUXVjGpgIbH3H5XT/introducing-the-g7-network/world-builder', icon: <IconDocumentation /> },
+  { name: 'documentation', navigateTo: 'https://wiki.game7.io/g7-developer-resource/bWmdEUXVjGpgIbH3H5XT/', icon: <IconDocumentation /> },
 ]
 
 const MainLayout: React.FC<MainLayoutProps> = ({}) => {
