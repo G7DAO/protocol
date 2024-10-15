5dfa0d6bf81ab25a83bebaa7e4eb111f0d7796d3
Memo Khoury <majd.khoury93@gmail.com>
2024-10-15 23:23:16 +0100

    links

--- webapps/world-builder-dashboard/src/layouts/MainLayout/DesktopSidebar.tsx
+++ webapps/world-builder-dashboard/src/layouts/MainLayout/DesktopSidebar.tsx
@@ -4,6 +4,7 @@ import styles from './MainLayout.module.css'
 import IconLogout from '@/assets/IconLogout'
 import { useBlockchainContext } from '@/contexts/BlockchainContext'
 import Game7Logo from '@/layouts/MainLayout/Game7Logo'
+import IconLinkExternal02 from '@/assets/IconLinkExternal02'
 
 interface DesktopSidebarProps {
   navigationItems: { name: string; navigateTo: string; icon: ReactNode }[]
@@ -21,11 +22,26 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ navigationItems }) => {
           {navigationItems.map((item) => (
             <div
               className={location.pathname.startsWith(item.navigateTo) ? styles.selectedNavButton : styles.navButton}
-              onClick={() => navigate(item.navigateTo)}
+              onClick={() => {
+                if (item.name === 'documentation' || item.name === 'explorer') {
+                  window.open(item.navigateTo, '_blank')
+                } else {
+                  navigate(item.navigateTo)
+                }
+              }}
               key={item.name}
             >
-              {item.icon}
-              {item.name}
+              <div className={styles.navBeginning}>
+                {item.icon}
+                {item.name}
+              </div>
+              <div style={{ display: 'flex' }}>
+                {item.name === 'documentation' || item.name === 'explorer' ? (
+                  <IconLinkExternal02 className={styles.icon} />
+                ) : (
+                  ''
+                )}
+              </div>
             </div>
           ))}
         </div>
--- webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
+++ webapps/world-builder-dashboard/src/layouts/MainLayout/MainLayout.tsx
@@ -1,22 +1,28 @@
 // React and related libraries
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
 // Styles
 import styles from './MainLayout.module.css'
 import IconDroplets02 from '@/assets/IconDroplets02'
 import IconWallet04 from '@/assets/IconWallet04'
-
 // Local components and assets
 import DesktopSidebar from '@/layouts/MainLayout/DesktopSidebar'
 import MobileSidebar from '@/layouts/MainLayout/MobileSidebar'
 import { useMediaQuery } from '@mantine/hooks'
+import IconExplorer from '@/assets/IconExplorer'
+import IconDocumentation from '@/assets/IconDocumentation'
 
 interface MainLayoutProps {}
 
 const NAVIGATION_ITEMS = [
   { name: 'bridge', navigateTo: '/bridge', icon: <IconWallet04 /> },
-  { name: 'faucet', navigateTo: '/faucet', icon: <IconDroplets02 /> }
+  { name: 'faucet', navigateTo: '/faucet', icon: <IconDroplets02 /> },
+  { name: 'explorer', navigateTo: 'https://testnet.game7.io/', icon: <IconExplorer /> },
+  {
+    name: 'documentation',
+    navigateTo: 'https://wiki.game7.io/g7-developer-resource/bWmdEUXVjGpgIbH3H5XT/',
+    icon: <IconDocumentation />
+  }
 ]
 
 const MainLayout: React.FC<MainLayoutProps> = ({}) => {
