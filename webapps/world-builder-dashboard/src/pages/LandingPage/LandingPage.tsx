5424af04802545520670b06d69bdb7bcba7b5ce0
Memo Khoury <majd.khoury93@gmail.com>
2024-10-18 16:22:50 +0100

    styling

--- webapps/world-builder-dashboard/src/pages/LandingPage/LandingPage.tsx
+++ webapps/world-builder-dashboard/src/pages/LandingPage/LandingPage.tsx
@@ -134,7 +134,7 @@ const LandingPage: React.FC<LandingPageProps> = () => {
                 <div className={styles.contentContainer}>
                   <div className={styles.pill}>DEVHUB</div>
                   <div className={styles.titleContainer}>
-                    <div className={styles.titleText}>BUILD YOUR GAME</div>
+                    <div className={styles.titleText}>Build Your Game</div>
                     <div className={styles.subtitleText}>Be a part of the future of gaming</div>
                   </div>
                   {!smallView && (<div className={styles.ctaContainer}>
