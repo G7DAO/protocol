5eb42740b0518c4efaf2f8c4e0bbf90bce08bc86
Anton Mushnin <anton.mushnin@gmail.com>
2024-10-26 11:11:18 +0300

    feat: using local storage for transactions history

--- webapps/game7-bridge-example/src/components/BridgeView.tsx
+++ webapps/game7-bridge-example/src/components/BridgeView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { ethers } from 'ethers'
-import { useQuery, useMutation } from 'react-query'
+import {useQuery, useMutation, useQueryClient} from 'react-query'
 
 import styles from './BridgeView.module.css'
 import { useWallet } from '../contexts/WalletContext'
@@ -49,7 +49,7 @@ const BridgerView = ({ bridger, amount, hoveredItem }: { bridger: Bridger; amoun
 
 
 
-
+  const queryClient = useQueryClient()
   const transfer = useMutation({
     mutationFn: async (amount: string) => {
       const network = NETWORKS.find((n) => n.chainId === bridger.originNetwork.chainId)
@@ -59,19 +59,21 @@ const BridgerView = ({ bridger, amount, hoveredItem }: { bridger: Bridger; amoun
       return bridger.transfer({ amount: ethers.utils.parseUnits(amount), signer, destinationProvider })
     },
     onSuccess: (data) => {
-      console.log(data)
-      //0x5ce56d7cf0554bec609e995f3f3e98ad495a08fe29e66b91a9d951840fce6674
-      // 0x68bb539766ba5fcc6eba8536eaa5ac3f7e346e2eab8b6ebbaa9a976d6b8786ec
-      //0x630d46c87e1df9ab91b8f6311b711033fc11b95ef3487135af5e0726506f4135
-      // 0xc2c6cff17958df71b2507aa41393d9085ad4492b631271a590fdc7a834cb4275 L2->L3
-      // 0x9a0d867b6523f1d55bbb0d1b16779c5cb433744f646bd9209f382142bb10ec06 L2->L1
-      // 0x4f7aaf3d84d69a27123523deb85827982143c83fe6862eb1dca8c5ebef369740 L3->L2
-      // 0x4d011728e4b8002750a0dcb8f2b18d7f17a08c278acda381d26d6eb9c460157f L3->L2
-      // 0x8460187b8602c2cf2436f7821836c9097182d550a31cefaa07fb6352c013981e L2->L3
-      // 0x6db1d677bc87d64d0adf0ef89d059e0a6b9a8f765af5dc0d2977f9d87aaf8677 L2->L1 ETH
-      // 0xb5ba500f030e662a3bd4742c8f090b819881c508c9b748d699cf7820253afea8 L2->L1 ETH
-      // 0x5311d470b7956262ad3329ea75bebb9fc01d9f07dde419e4dbcddae440215415 L2->L1 TG7T
-      // 0x3b3581e5000f84ddfd22e61e6a800a01ceba6246fb2042816967a0034978e9ec L2->L1 TG7T
+      const record = {txHash: data.hash, originNetworkChainId: bridger.originNetwork.chainId, destinationNetworkChainId: bridger.destinationNetwork.chainId}
+      const historyJSON = localStorage.getItem(`${account}-bridge_0.0.1`)
+      let history
+      try {
+        if (!historyJSON) {
+          history = []
+        } else {
+          history = JSON.parse(historyJSON)
+        }
+        history.push(record)
+        localStorage.setItem(`${account}-bridge_0.0.1`, JSON.stringify(history))
+      } catch (e) {
+        console.error('writing to the local storage failed', e)
+      }
+      queryClient.invalidateQueries('history')
     }
   })
 
@@ -320,22 +322,18 @@ const Network = ({ network, hoveredItem, historyInfo }: { network: BridgeNetwork
 }
 
 const BridgeView = () => {
-  const networks = [L1_NETWORK, L2_NETWORK].map((n) => new BridgeNetwork(n.chainId, [TG7T, ETH]))
+  const networks = [L1_NETWORK, L2_NETWORK, L3_NETWORK].map((n) => new BridgeNetwork(n.chainId, [TG7T, ETH]))
   const { account } = useWallet()
 
   const history = useQuery(["history", account], async () => {
-    const transactions = [
-      // {txHash: '0x2fce35c0ea706b1fa2a261453782c2adf11b1c4b6e8b230859ca6010fcb2fcd8', originNetworkChainId: 11155111, destinationNetworkChainId: 421614}, //L1->L2 ETH
-      // {txHash: '0x9957a9a1b479e2cbc9dc95e15bd737d85301b31ad45d2a007fc9b3ba1870aa23', originNetworkChainId: 11155111, destinationNetworkChainId: 421614}, //L1->L2 TG7
-      // {txHash: '0x05b4024deff28ca7b29336c49ca341f403d4a5be2e72717f3250b2a13ce11d2c', originNetworkChainId: 421614, destinationNetworkChainId: 13746 }, //L2->L3 TG7
-      // {txHash: '0xae64c39527cfa697f5e3a2b56d31b9ee7fdd852678f787e4ab0e4e020033de29', originNetworkChainId: 13746, destinationNetworkChainId: 421614 }, //L3->L2 TG7
-      // {txHash: '0xb5ba500f030e662a3bd4742c8f090b819881c508c9b748d699cf7820253afea8', originNetworkChainId: 421614, destinationNetworkChainId: 11155111}, //L2->L1 ETH
-      {txHash: '0x8f38d4631383f2effd4722b09bf071ecf00562b694a14cf3eed955bcc43870c1', originNetworkChainId: 11155111, destinationNetworkChainId: 421614}, //L1->L2 TG7
-      {txHash: '0xd6be57ecb03321ef2e50ad124e2aa887792414608976d3d5d3aea0676fff0497', originNetworkChainId: 421614, destinationNetworkChainId: 11155111}, //L2->L1 TG7
-      {txHash: '0x27b967cbad2dfdbced7bf1a850f3f913e35779f751058d2520d0aed5593d8514', originNetworkChainId: 421614, destinationNetworkChainId: 11155111}, //L2->L1 TG7
-      {txHash: '0x4704707a0f19c8303afa8dc32d901a1acc42e002c2cedfae4c438ffb6411a323', originNetworkChainId: 421614, destinationNetworkChainId: 11155111}, //L2->L1 TG7
-    ];
-    return transactions
+    const historyJSON = localStorage.getItem(`${account}-bridge_0.0.1`)
+    let history
+    try {
+      history = JSON.parse(historyJSON)
+    } catch (e) {
+      console.error("can't read transfers history", e)
+    }
+    return history
   })
 
   const historyInfo = useQuery(["historyInfo", account, history.data], async () => {
