ad1d9e5662511561745cf7cb12b45c91193adc4d
Memo Khoury <majd.khoury93@gmail.com>
2024-10-28 18:48:43 +0000

    tokenSelector remove

--- webapps/world-builder-dashboard/src/components/commonComponents/tokenSelector/TokenSelector.tsx
+++ webapps/world-builder-dashboard/src/components/commonComponents/tokenSelector/TokenSelector.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import { ALL_NETWORKS } from '../../../../constants'
 import styles from './TokenSelector.module.css'
-import { ethers } from 'ethers'
 import { Combobox, Group, InputBase, InputBaseProps, useCombobox } from 'summon-ui/mantine'
 import IconCheck from '@/assets/IconCheck'
 import IconChevronDown from '@/assets/IconChevronDown'
 import { useBlockchainContext } from '@/contexts/BlockchainContext'
 import { Token } from '@/utils/tokens'
-import { doesContractExist } from '@/utils/web3utils'
 
 type TokenSelectorProps = {
   tokens: Token[]
@@ -17,53 +13,12 @@ type TokenSelectorProps = {
   selectedChainId: number
 } & InputBaseProps
 
-const TokenSelector = ({ tokens, onChange, selectedToken, onTokenAdded, selectedChainId }: TokenSelectorProps) => {
+const TokenSelector = ({ tokens, onChange, selectedToken }: TokenSelectorProps) => {
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption()
   })
 
-  // const [tokenAddress, setTokenAddress] = useState<string>('')
-  // const [error, setError] = useState<string>('')
   const { selectedHighNetwork, selectedLowNetwork } = useBlockchainContext()
-
-  // const handleTokenInput = (tokenAddress: string) => {
-  //   setTokenAddress(tokenAddress)
-
-  //   let web3Provider
-  //   if (window.ethereum) web3Provider = new ethers.providers.Web3Provider(window.ethereum)
-  //   else throw new Error('Wallet is not installed')
-
-  //   if (!ethers.utils.isAddress(tokenAddress)) setError('Not an address!')
-  //   else if (!doesContractExist(tokenAddress, web3Provider)) setError(`Contract doesn't exist!`)
-  //   else if (tokens.find((token) => token.address === tokenAddress)) setError('Token already exists')
-  //   else setError('')
-  // }
-
-  // const addToken = (tokenAddress: string) => {
-  //   try {
-  //     if (error !== '') return
-  //     const storageKey = `${connectedAccount}-${selectedChainId}`
-  //     const existingTokens = JSON.parse(localStorage.getItem(storageKey) || '[]')
-  //     const rpc = ALL_NETWORKS.find((network) => network.chainId === selectedChainId)?.rpcs[0]
-  //     const token = {
-  //       name: `${tokenAddress.slice(0, 6)}`,
-  //       symbol: `${tokenAddress.slice(0, 6)}`,
-  //       address: tokenAddress,
-  //       rpc: rpc
-  //     }
-
-  //     if (tokens.find((token: Token) => token.address === tokenAddress)) {
-  //       setError("Can't add the same token!")
-  //       return
-  //     }
-  //     const updatedTokens = [...existingTokens, token]
-  //     localStorage.setItem(storageKey, JSON.stringify(updatedTokens))
-  //     onTokenAdded()
-  //   } catch (err) {
-  //     console.log(err)
-  //     setError(`` + err)
-  //   }
-  // }
 
   return (
     <Combobox
@@ -119,28 +74,6 @@ const TokenSelector = ({ tokens, onChange, selectedToken, onTokenAdded, selected
             )
           })}
         </Combobox.Options>
-        <div className={styles.tokenAdderLayout}>
-          <div className={styles.tokenAdderContainer}>
-            {error && error.length > 0 && (
-              <div className={styles.tokenAdderError}>
-                <div className={styles.tokenErrorText}>{error}</div>
-              </div>
-            )}
-            {/* <div className={styles.tokenAdder}>
-              <input
-                className={`${styles.tokenAddressInput} ${error && error.length > 0 ? styles.error : ''}`}
-                value={tokenAddress}
-                placeholder='Import token address'
-                onChange={(e) => {
-                  handleTokenInput(e.target.value)
-                }}
-              />
-              <div className={styles.importButton} onClick={() => addToken(tokenAddress)}>
-                <div className={styles.importText}>Import</div>
-              </div>
-            </div> */}
-          </div>
-        </div>
       </Combobox.Dropdown>
     </Combobox>
   )
