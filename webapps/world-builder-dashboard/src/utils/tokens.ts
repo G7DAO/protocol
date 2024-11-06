e30a99d78659c0cb0b156c4782ec5cc1140f82b7
Memo Khoury <majd.khoury93@gmail.com>
2024-11-06 11:43:42 +0000

    added usdc

--- webapps/world-builder-dashboard/src/utils/tokens.ts
+++ webapps/world-builder-dashboard/src/utils/tokens.ts
@@ -1,9 +1,10 @@
-import { ETH, L1_NETWORK, L2_NETWORK, L3_NETWORK, TG7T } from '../../constants'
+import { ETH, L1_NETWORK, L2_NETWORK, L3_NETWORK, TG7T, USDC } from '../../constants'
 import { ZERO_ADDRESS } from './web3utils'
+import { TokenAddressMap } from 'game7-bridge-sdk'
 import IconEthereum from '@/assets/IconEthereum'
 import IconG7T from '@/assets/IconG7T'
 import IconTokenNoSynbol from '@/assets/IconTokenNoSymbol'
-import { TokenAddressMap } from 'game7-bridge-sdk'
+import IconUSDC from '@/assets/IconUSDC'
 
 export interface Token {
   name: string
@@ -43,6 +44,15 @@ export const getTokensForNetwork = (chainId: number | undefined, userAddress: st
           tokenAddressMap: ETH,
           chainId: L1_NETWORK.chainId
         },
+        {
+          name: 'USDC',
+          symbol: 'USDC',
+          address: '0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8',
+          Icon: IconUSDC,
+          rpc: L1_NETWORK.rpcs[0],
+          tokenAddressMap: USDC,
+          chainId: L1_NETWORK.chainId
+        },
         ...storedTokensWithItems
       ]
     case L2_NETWORK.chainId:
@@ -65,6 +75,15 @@ export const getTokensForNetwork = (chainId: number | undefined, userAddress: st
           tokenAddressMap: ETH,
           chainId: L2_NETWORK.chainId
         },
+        {
+          name: 'USDC',
+          symbol: 'USDC',
+          address: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+          Icon: IconUSDC,
+          rpc: L2_NETWORK.rpcs[0],
+          tokenAddressMap: USDC,
+          chainId: L2_NETWORK.chainId
+        },
         ...storedTokensWithItems
       ]
     case L3_NETWORK.chainId:
@@ -76,6 +95,15 @@ export const getTokensForNetwork = (chainId: number | undefined, userAddress: st
           Icon: IconG7T,
           rpc: L3_NETWORK.rpcs[0],
           tokenAddressMap: TG7T,
+          chainId: L3_NETWORK.chainId
+        },
+        {
+          name: 'USDC',
+          symbol: 'USDC',
+          address: '0x6a0D75EF95d21f1Ba5e2619fBc9D15F424d130BF',
+          Icon: IconUSDC,
+          rpc: L3_NETWORK.rpcs[0],
+          tokenAddressMap: USDC,
           chainId: L3_NETWORK.chainId
         },
         ...storedTokensWithItems
