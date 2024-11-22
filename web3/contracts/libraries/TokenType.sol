7393cecd06512dc6bf4e85236ae1b1d1c9ff6258
mrk-hub <mrkennedy@game7.io>
2024-11-22 10:28:17 -0500

    Added TokenType.sol

---
+++ web3/contracts/libraries/TokenType.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+library TokenType {
+    function native_token_type() internal pure returns (uint256) {
+        return 1;
+    }
+
+    function erc20_type() internal pure returns (uint256) {
+        return 20;
+    }
+
+    function erc721_type() internal pure returns (uint256) {
+        return 721;
+    }
+
+    function erc1155_type() internal pure returns (uint256) {
+        return 1155;
+    }
+}
