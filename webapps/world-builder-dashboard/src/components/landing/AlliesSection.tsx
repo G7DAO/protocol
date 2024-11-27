1a2dca7d975c144f9977493197e2cf4e43b8f1cd
Memo Khoury <majd.khoury93@gmail.com>
2024-11-27 14:09:35 +0000

    links

--- webapps/world-builder-dashboard/src/components/landing/AlliesSection.tsx
+++ webapps/world-builder-dashboard/src/components/landing/AlliesSection.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import styles from './Landing.module.css';
-import { HyperPlayLogo, SummonLogo, SummonTextLogo, ArbitrumLogo, ConduitLogo, MarketWarsLogo } from '../../assets';
+import React from 'react'
+import { HyperPlayLogo, SummonLogo, SummonTextLogo, ArbitrumLogo, ConduitLogo, MarketWarsLogo } from '../../assets'
+import styles from './Landing.module.css'
+
+const allies = [
+  {
+    logo: <HyperPlayLogo />,
+    link: 'https://www.hyperplay.xyz/'
+  },
+  {
+    logo: (
+      <>
+        <SummonLogo /> <SummonTextLogo />
+      </>
+    ),
+    link: 'https://summon.xyz/'
+  },
+  {
+    logo: <ArbitrumLogo />,
+    link: 'https://arbitrum.io/'
+  },
+  {
+    logo: <ConduitLogo />,
+    link: 'https://www.conduit.xyz/'
+  },
+  {
+    logo: <MarketWarsLogo />,
+    link: 'https://www.marketwars.com/'
+  }
+]
 
 const AlliesSection: React.FC = () => {
-    return (
-        <div className={styles.thirdSection}>
-            <div className={styles.contentContainer}>
-                <div className={styles.sectionTitle}>G7 Network allies</div>
-                <div className={styles.sponsorCards}>
-                    <div className={styles.sponsorCard}><HyperPlayLogo /></div>
-                    <div className={styles.sponsorCard}><SummonLogo /><SummonTextLogo /></div>
-                    <div className={styles.sponsorCard}><ArbitrumLogo /></div>
-                    <div className={styles.sponsorCard}><ConduitLogo /></div>
-                    <div className={styles.sponsorCard}><MarketWarsLogo /></div>
-                </div>
+  return (
+    <div className={styles.thirdSection}>
+      <div className={styles.contentContainer}>
+        <div className={styles.sectionTitle}>G7 Network allies</div>
+        <div className={styles.sponsorCards}>
+          {allies.map((ally) => (
+            <div className={styles.sponsorCard} onClick={() => window.open(ally.link, '_blank')}>
+              {ally.logo}
             </div>
+          ))}
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default AlliesSection;
+export default AlliesSection
