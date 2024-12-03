cf981165a489ece1f4dca035c517ebc7ebef9fc7
Memo Khoury <majd.khoury93@gmail.com>
2024-12-03 10:24:07 +0000

    <strong>

--- webapps/world-builder-dashboard/src/components/landing/BenefitsSection.tsx
+++ webapps/world-builder-dashboard/src/components/landing/BenefitsSection.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import styles from './Landing.module.css';
+import React from 'react'
+import styles from './Landing.module.css'
 
 const benefits = [
-    {
-        title: "Build for gamers",
-        imageClass: styles.cardImageGamers,
-        description: "Bootstrap your game with access to 250k+ citizens and counting",
-    },
-    {
-        title: "Fast and efficient",
-        imageClass: styles.cardImageLightningQuick,
-        description: "Lighting-quick transactions and low cost fees",
-    },
-    {
-        title: "Special economic zone",
-        imageClass: styles.cardImageSpecialEcon,
-        description: "Access World Builder’s powerful developer tools",
-    },
-];
+  {
+    title: 'Build for gamers',
+    imageClass: styles.cardImageGamers,
+    description: '<p>Bootstrap your game with access to <span style="font-weight:bold;">250,000+</span> citizens and counting</p>'
+  },
+  {
+    title: 'Fast and efficient',
+    imageClass: styles.cardImageLightningQuick,
+    description: 'Lighting-quick transactions and low cost fees'
+  },
+  {
+    title: 'Special economic zone',
+    imageClass: styles.cardImageSpecialEcon,
+    description: 'Access World Builder’s powerful developer tools'
+  }
+]
 
 const BenefitsSection: React.FC = () => {
-    return (
-        <div className={styles.secondSection}>
-            <div className={styles.contentContainer}>
-                <div className={styles.sectionTitle}>Get all of the benefits of the G7 Network</div>
-                <div className={styles.cards}>
-                    {benefits.map((benefit, index) => (
-                        <div className={styles.card} key={index}>
-                            <div className={styles.cardTitle}>{benefit.title}</div>
-                            <div className={`${styles.cardImage} ${benefit.imageClass}`} />
-                            <div className={styles.cardDescription}>{benefit.description}</div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div className={styles.secondSection}>
+      <div className={styles.contentContainer}>
+        <div className={styles.sectionTitle}>Get all of the benefits of the G7 Network</div>
+        <div className={styles.cards}>
+          {benefits.map((benefit, index) => (
+            <div className={styles.card} key={index}>
+              <div className={styles.cardTitle}>{benefit.title}</div>
+              <div className={`${styles.cardImage} ${benefit.imageClass}`} />
+              <div className={styles.cardDescription} dangerouslySetInnerHTML={{ __html: benefit.description }} />
             </div>
+          ))}
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default BenefitsSection;
+export default BenefitsSection
