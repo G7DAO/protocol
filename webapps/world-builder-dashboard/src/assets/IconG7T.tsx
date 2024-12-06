405ee4b16f73acebdbffc1b76bb1faed422d59cc
Memo Khoury <majd.khoury93@gmail.com>
2024-12-06 17:47:29 +0000

    update g7 icon

--- webapps/world-builder-dashboard/src/assets/IconG7T.tsx
+++ webapps/world-builder-dashboard/src/assets/IconG7T.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 
 const IconG7T: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 20 20' fill='none' {...props}>
-    <path
-      d='M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10Z'
-      fill='#FE7071'
-    />
-    <path
-      d='M4.33333 6.16667L6.62944 9.76822H9.60412L8.92848 8.70799H11.0727L8.51324 12.7238L10 15.0556L15.6667 6.16667H4.33333Z'
-      fill='#1B1B1B'
-    />
-  </svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20" fill="none" {...props}>
+  <path d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10Z" fill="#FE7071"/>
+  <path d="M4.33333 6.16667L6.62944 9.76822H9.60412L8.92848 8.70799H11.0727L8.51324 12.7238L10 15.0556L15.6667 6.16667H4.33333Z" fill="white"/>
+</svg>
 )
 
 export default IconG7T
