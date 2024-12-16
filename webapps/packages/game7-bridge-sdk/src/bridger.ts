63107d04c5ac8cf054a6b045cdd76c94aa732825
Anton Mushnin <anton.mushnin@gmail.com>
2024-12-16 23:53:13 +0300

    refactor: moving Bridger factory to standalone module

--- webapps/packages/game7-bridge-sdk/src/bridger.ts
+++ webapps/packages/game7-bridge-sdk/src/bridger.ts
@@ -206,14 +206,8 @@ export class Bridger {
   }
 
 
-  static getBridger(originNetworkChainId: number,
-                    destinationNetworkChainId: number,
-                    token: TokenAddressMap,
-                    params?: { useLocalStorage?: boolean; approveDepositAllowance?: boolean }) {
-    if (isNativeUSDC(originNetworkChainId, token[originNetworkChainId])) {
-      return new CctpBridger(originNetworkChainId, destinationNetworkChainId, token, params)
-    }
-    return new Bridger(originNetworkChainId, destinationNetworkChainId, token, params)
+  public isCctp(): boolean  {
+    return false
   }
 
   /**
