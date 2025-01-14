fb21bb4e848cfa9ad3bc4c06942ba091e304a469
Memo Khoury <majd.khoury93@gmail.com>
2025-01-14 15:52:58 +0000

    update useCoinGeckoAPI

--- webapps/world-builder-dashboard/src/hooks/useCoinGeckoAPI.ts
+++ webapps/world-builder-dashboard/src/hooks/useCoinGeckoAPI.ts
@@ -1,13 +1,13 @@
 import { useQuery } from 'react-query'
-const BASE_URL = 'https://api.coingecko.com/api/v3/simple/price'
 
 export const useCoinGeckoAPI = () => {
   const useUSDPriceOfToken = (coin: string) => {
     return useQuery(
       ['priceCrypto', coin],
       async () => {
-        const res = await fetch(`${BASE_URL}?ids=${coin}&vs_currencies=usd`, {
-          method: 'GET'
+        const res = await fetch(`https://pro-api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`, {
+          method: 'GET',
+          headers: { accept: 'application/json', 'x-cg-pro-api-key': import.meta.env.VITE_COIN_GECKO_API_KEY }
         })
         if (!res.ok) {
           throw new Error(`Error: ${res.statusText}`)
