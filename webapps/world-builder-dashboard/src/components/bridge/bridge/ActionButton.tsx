13a06e0f1fb51f9bb5584018cba0a2d6ba07be0d
Memo Khoury <majd.khoury93@gmail.com>
2025-02-04 22:17:01 +0000

    update condition

--- webapps/world-builder-dashboard/src/components/bridge/bridge/ActionButton.tsx
+++ webapps/world-builder-dashboard/src/components/bridge/bridge/ActionButton.tsx
@@ -1,5 +1,5 @@
 // External Libraries
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 // Constants
@@ -68,6 +68,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({
   const [showApproval, setShowApproval] = useState(false)
   const [startingTokenIndex, setStartingTokenIndex] = useState(0)
 
+
   const allowances = useAllowances({
     bridger,
     direction,
@@ -75,6 +76,19 @@ const ActionButton: React.FC<ActionButtonProps> = ({
     selectedHighNetwork,
     connectedAccount: connectedAccount ?? ''
   })
+
+
+  useEffect(() => {
+    console.log({
+      label: getLabel() === 'Submit',
+      isDisabled: isDisabled,
+      amount: Number(amount) < 0,
+      l2l3message: (!L2L3message?.destination || !L2L3message.data),
+      isfetching: isFetchingGasFee,
+      gasFeesBalance: Number(gasFees?.[1]) > Number(nativeBalance),
+      allowances: allowances.isLoading
+    })
+  }, [isDisabled, amount, L2L3message, L2L3message, allowances, isFetchingGasFee, gasFees, nativeBalance])
 
   const checkAllowances = async (): Promise<boolean> => {
     if (!bridger || !connectedAccount) return false
@@ -291,7 +305,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({
           (isDisabled ||
             Number(amount) < 0 ||
             ((!L2L3message?.destination || !L2L3message.data) && Number(amount) === 0)) ||
-          allowances?.isLoading || isFetchingGasFee || Number(gasFees?.[1]) > Number(nativeBalance)
+          allowances?.isLoading || isFetchingGasFee || Number(gasFees?.[1]) < Number(nativeBalance)
         }
       >
         <div className={isConnecting || transfer.isPending ? styles.buttonLabelLoading : styles.buttonLabel}>
