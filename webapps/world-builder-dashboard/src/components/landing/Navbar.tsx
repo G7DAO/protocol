050ab286ecd97e7e9de65cf8ac7f5be49a8326a8
akgameseven <ak@game7.io>
2025-02-11 00:15:30 +0300

    status page

--- webapps/world-builder-dashboard/src/components/landing/Navbar.tsx
+++ webapps/world-builder-dashboard/src/components/landing/Navbar.tsx
@@ -1,10 +1,9 @@
 // Navbar.tsx
 import React from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import styles from './Landing.module.css'
 import IconGame7 from '@/assets/IconGame7'
 import IconGame7Logo from '@/assets/IconGame7Logo'
-import IconHamburgerLanding from '@/assets/IconHamburgerLanding'
 
 interface NavbarProps {
   navbarOpen: boolean
@@ -16,20 +15,8 @@ interface NavbarProps {
   isSticky: boolean
 }
 
-const NAVBAR_ITEMS = [
-  { name: 'Home', link: '/' },
-  { name: 'Faucet', link: 'faucet' },
-  { name: 'Bridge', link: 'bridge' },
-  { name: 'Community', link: 'https://discord.com/invite/g7dao' },
-  {
-    name: 'Docs',
-    link: 'https://docs.game7.io/'
-  }
-]
-
-const Navbar: React.FC<NavbarProps> = ({ navbarOpen, smallView, setIsNavbarOpen, startBuilding, navigateLink, isContainer, isSticky }) => {
+const Navbar: React.FC<NavbarProps> = ({ isContainer, isSticky }) => {
   const navigate = useNavigate()
-  const location = useLocation()
   return (
     <>
       <div className={`${styles.navbarContainer} 
@@ -40,52 +27,9 @@ const Navbar: React.FC<NavbarProps> = ({ navbarOpen, smallView, setIsNavbarOpen,
             <IconGame7Logo />
             <IconGame7 />
           </div>
-          <div className={styles.navbarItemsContainer}>
-            {!smallView ? (
-              <div className={styles.navbarItems}>
-                {NAVBAR_ITEMS.map((item, index) => (
-                  <div
-                    key={index}
-                    className={((location.pathname === '/' || location.pathname === '') && item.name === 'Home') ? styles.navbarItemHome : styles.navbarItem}
-                    onClick={() => {
-                      navigateLink(item)
-                    }}
-                  >
-                    {item.name}
-                  </div>
-                ))}
-                <div className={styles.navbarCTA} onClick={startBuilding}>
-                  Start building
-                </div>
-              </div>
-            ) : (
-              <div className={styles.navbarItem}>
-                <IconHamburgerLanding onClick={() => setIsNavbarOpen(!navbarOpen)} />
-              </div>
-            )}
-          </div>
         </div>
       </div>
 
-      {/* Expanded Navbar for small view */}
-      {navbarOpen && smallView && (
-        <div className={styles.navContainer}>
-          {NAVBAR_ITEMS.map((item, index) => (
-            <div
-              key={index}
-              className={item.name === 'Home' ? styles.navItemHome : styles.navItem}
-              onClick={() => navigateLink(item)}
-            >
-              {item.name}
-            </div>
-          ))}
-          <div className={styles.ctaContainer}>
-            <div className={styles.navbarCTA} onClick={startBuilding}>
-              Start building
-            </div>
-          </div>
-        </div>
-      )}
     </>
   )
 }
