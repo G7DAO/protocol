099c02f413c59b4586d020d7e5185eb8abad4d35
akgameseven <ak@game7.io>
2025-02-10 23:38:19 +0300

    status page

--- webapps/world-builder-dashboard/src/assets/IconExternalLink.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IconExternalLink: React.FC<React.SVGProps<SVGSVGElement>> = ((props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" {...props}>
-        <path d="M17.5 7.50001L17.5 2.50001M17.5 2.50001H12.5M17.5 2.50001L10 10M8.33333 2.5H6.5C5.09987 2.5 4.3998 2.5 3.86502 2.77248C3.39462 3.01217 3.01217 3.39462 2.77248 3.86502C2.5 4.3998 2.5 5.09987 2.5 6.5V13.5C2.5 14.9001 2.5 15.6002 2.77248 16.135C3.01217 16.6054 3.39462 16.9878 3.86502 17.2275C4.3998 17.5 5.09987 17.5 6.5 17.5H13.5C14.9001 17.5 15.6002 17.5 16.135 17.2275C16.6054 16.9878 16.9878 16.6054 17.2275 16.135C17.5 15.6002 17.5 14.9001 17.5 13.5V11.6667" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-))
-
-export default IconExternalLink
