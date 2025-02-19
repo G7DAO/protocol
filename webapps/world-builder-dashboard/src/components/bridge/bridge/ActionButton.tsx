cfd409ccc7078c91fb3931f0f1128ca9b826d5c4
Memo Khoury <majd.khoury93@gmail.com>
2025-02-19 12:02:34 +0000

    small fix

--- webapps/world-builder-dashboard/src/components/bridge/bridge/ActionButton.tsx
+++ webapps/world-builder-dashboard/src/components/bridge/bridge/ActionButton.tsx
@@ -209,7 +209,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({
           highNetworkChainId: selectedHighNetwork.chainId,
           highNetworkHash: tx?.hash,
           highNetworkTimestamp: Date.now() / 1000,
-          challengePeriod: selectedNetworkType === 'Testnet' ? 60 * 60 : selectedLowNetwork.chainId === 42161 && selectedBridgeToken.symbol === 'USDC' ? 60 * 15 :
+          challengePeriod: selectedNetworkType === 'Testnet' ? 60 * 60 :
             selectedBridgeToken.symbol === 'USDC' && (selectedHighNetwork.chainId === 42161) ?
               60 * 15 : selectedHighNetwork.chainId === 42161 ? 60 * 60 * 24 * 7 : 60 * 60,
           symbol: returnSymbol(direction, selectedHighNetwork, selectedLowNetwork, symbol ?? ''),
